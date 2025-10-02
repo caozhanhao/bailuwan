70b2d18ae77810367c7fc9f87a794de53c1be990
tracer-ysyx <tracer@ysyx.org>
2025-10-02 22:07:55 +0800

    > compile NEMU ysyx_22040000 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  22:07:55 up 12:26,  1 user,  load average: 3.15, 3.05, 2.91

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -74,7 +74,7 @@ static struct rule {
      * Pay attention to the precedence level of different rules.
      */
 
-    {"[ \t\n]+", TK_NOTYPE},
+    {" ", TK_NOTYPE},
 
     {"&&", TK_LAND},
     {"\\|\\|", TK_LOR},
