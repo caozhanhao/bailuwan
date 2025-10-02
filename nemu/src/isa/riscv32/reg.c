adfa6664d854f174298d21d6246adf938f63a8a4
tracer-ysyx <tracer@ysyx.org>
2025-10-02 17:03:03 +0800

    > compile NEMU ysyx_22040000 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  17:03:03 up  7:21,  1 user,  load average: 1.89, 2.17, 1.99

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -25,7 +25,7 @@ const char *regs[] = {
 
 void isa_reg_display() {
   for (int i = 0; i < 32; i++)
-    printf("%s\t0x%x\t%d\n", regs[i], gpr(i), gpr(i));
+    printf("%-5s  0x%08x  %11d\n", regs[i], gpr(i), gpr(i));
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
