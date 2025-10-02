f394709775604d0de415856d15525cb4a52be28a
tracer-ysyx <tracer@ysyx.org>
2025-10-02 21:58:06 +0800

    > compile NEMU ysyx_22040000 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  21:58:06 up 12:16,  1 user,  load average: 3.39, 3.42, 2.94

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -1,17 +1,19 @@
 /***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan
+ *PSL v2. You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+ *KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+ *NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
+
+#include "monitor/sdb/sdb.h"
 
 #include <common.h>
 
@@ -20,16 +22,66 @@ void am_init_monitor();
 void engine_start();
 int is_exit_status_bad();
 
+int test_expr_eval(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("Usage: %s input_file\n", argv[0]);
+    return 1;
+  }
+
+  FILE *fp = fopen(argv[1], "r");
+  if (fp == NULL) {
+    perror("fopen");
+    return 1;
+  }
+
+  char *line = NULL;
+  size_t len = 0;
+  ssize_t nread;
+
+  int total = 0, passed = 0;
+
+  while ((nread = getline(&line, &len, fp)) != -1) {
+    if (nread <= 1) continue;
+
+    char *tok = strtok(line, " ");
+    if (tok == NULL) continue;
+
+    char* endptr;
+    unsigned expected = strtoul(tok, &endptr, 10);
+    Assert(tok != endptr, "invalid number");
+
+    tok = strtok(NULL, " ");
+
+    bool success = false;
+    word_t got = expr(tok, &success);
+
+    total++;
+    if (success && got == expected) {
+      passed++;
+    } else {
+      printf("[FAILED] expr: %s\n", tok);
+      printf("  expected: %u, got: %u\n", expected, got);
+    }
+  }
+
+  printf("Test finished: %d/%d passed.\n", passed, total);
+
+  free(line);
+  fclose(fp);
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
-  /* Initialize the monitor. */
-#ifdef CONFIG_TARGET_AM
-  am_init_monitor();
-#else
-  init_monitor(argc, argv);
-#endif
-
-  /* Start engine. */
-  engine_start();
-
-  return is_exit_status_bad();
+//   /* Initialize the monitor. */
+// #ifdef CONFIG_TARGET_AM
+//   am_init_monitor();
+// #else
+//   init_monitor(argc, argv);
+// #endif
+//
+//   /* Start engine. */
+//   engine_start();
+//
+//   return is_exit_status_bad();
+  return test_expr_eval(argc, argv);
 }
