4cbff062e589f5c464434b31476cc4b539ba1d6d
tracer-ysyx <tracer@ysyx.org>
2025-10-02 10:01:22 +0800

    > generate verilog ysyx_22040000 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  10:01:22 up 19 min,  1 user,  load average: 2.11, 2.61, 1.87

--- npc/playground/src/Top.scala
+++ npc/playground/src/Top.scala
@@ -3,32 +3,32 @@ package top
 import chisel3._
 
 class InBundle extends Bundle {
-  val btn = UInt(5.W)
-  val sw = UInt(16.W)
-  val ps2_clk = Bool()
+  val btn      = UInt(5.W)
+  val sw       = UInt(16.W)
+  val ps2_clk  = Bool()
   val ps2_data = Bool()
-  val uart_rx = Bool()
+  val uart_rx  = Bool()
 }
 
 class VGABundle extends Bundle {
-  val CLK = Clock()
-  val HSYNC = Bool()
-  val VSYNC = Bool()
+  val CLK     = Clock()
+  val HSYNC   = Bool()
+  val VSYNC   = Bool()
   val BLANK_N = Bool()
-  val R = UInt(8.W)
-  val G = UInt(8.W)
-  val B = UInt(8.W)
+  val R       = UInt(8.W)
+  val G       = UInt(8.W)
+  val B       = UInt(8.W)
 }
 
 class OutBundle extends Bundle {
   val uart_tx = Bool()
-  val ledr = UInt(16.W)
-  val vga = new VGABundle
-  val seg = Vec(8, UInt(8.W))
+  val ledr    = UInt(16.W)
+  val vga     = new VGABundle
+  val seg     = Vec(8, UInt(8.W))
 }
 
 class TopIO extends Bundle {
-  val in = Input(new InBundle)
+  val in  = Input(new InBundle)
   val out = Output(new OutBundle)
 }
 
