0b69d195ad952aa35df4f466a21f6b721e7e4f83
tracer-ysyx <tracer@ysyx.org>
2025-10-02 22:10:57 +0800

    > compile NEMU ysyx_22040000 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  22:10:57 up 12:29,  1 user,  load average: 3.44, 3.20, 2.99

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -74,7 +74,7 @@ static struct rule {
      * Pay attention to the precedence level of different rules.
      */
 
-    {"[ \\t\\n]+", TK_NOTYPE},
+    {" ", TK_NOTYPE},
 
     {"&&", TK_LAND},
     {"\\|\\|", TK_LOR},
@@ -154,8 +154,8 @@ static bool make_token(char *e) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i,
-            rules[i].regex, position, substr_len, substr_len, substr_start);
+        // Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i,
+        //     rules[i].regex, position, substr_len, substr_len, substr_start);
 
         position += substr_len;
 
