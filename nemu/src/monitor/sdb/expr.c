cdec367eca6a57e8657fa180b9d92a182f9b250b
tracer-ysyx <tracer@ysyx.org>
2025-10-02 20:43:01 +0800

    > compile NEMU ysyx_22040000 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  20:43:01 up 11:01,  1 user,  load average: 2.11, 2.61, 2.84

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -334,7 +334,7 @@ static word_t eval(int p, int q, bool *success) {
     }
 
     if (tokens[p].type == TK_REG) {
-      word_t res = isa_reg_str2val(tokens[p].str, success);
+      word_t res = isa_reg_str2val(tokens[p].str + 1, success);
       if (!*success)
         Log("eval: Bad register: %s", tokens[p].str);
       return res;
