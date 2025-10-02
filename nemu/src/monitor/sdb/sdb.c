ae78f1a6f7da455a9aa765e792f53c4ebb243053
tracer-ysyx <tracer@ysyx.org>
2025-10-03 00:15:45 +0800

    > compile NEMU ysyx_22040000 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  00:15:45 up 14:33,  1 user,  load average: 2.18, 2.27, 2.59

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -208,7 +208,7 @@ static int cmd_help(char *args) {
   if (arg == NULL) {
     /* no argument given */
     for (i = 0; i < NR_CMD; i++) {
-      printf("%-6s - %s\n", cmd_table[i].name, cmd_table[i].description);
+      printf("%-4s - %s\n", cmd_table[i].name, cmd_table[i].description);
     }
   } else {
     for (i = 0; i < NR_CMD; i++) {
