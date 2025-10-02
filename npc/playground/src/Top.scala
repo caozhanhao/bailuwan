9a1ce070cab4beae37728228d339a8af51397193
tracer-ysyx <tracer@ysyx.org>
2025-10-02 12:57:03 +0800

    > sim RTL ysyx_22040000 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  12:57:03 up  3:15,  1 user,  load average: 2.04, 2.41, 2.33

--- npc/playground/src/Top.scala
+++ npc/playground/src/Top.scala
@@ -36,4 +36,52 @@ class Top extends Module {
   val io = IO(new TopIO)
 
   io.out := 0.U.asTypeOf(io.out)
+
+  val code =
+    """
+      |li r0, 10        # load imm 10 for loop bound
+      |li r1, 0         # i = 0
+      |li r2, 0         # sum = 0
+      |li r3, 1         # load step imm 1 (step -> r3)
+      |
+      |loop:
+      |add r1, r1, r3   # i += 1
+      |add r2, r2, r1   # sum += i
+      |bner0 r1, loop   # branch to %4 if i != 10
+      |out r2           # set output as sum
+      |
+      |halt:
+      |bner0 r3, halt   # b 2 != 11, %7 (dead loop)
+      |""".stripMargin
+
+  val program = sCPU.assemble(code)
+  val cpu = Module(new sCPU(program))
+
+  val tbl = Seq(
+    0x7E,
+    0x30,
+    0x6D,
+    0x79,
+    0x33,
+    0x5B,
+    0x5F,
+    0x70,
+    0x7F,
+    0x7B,
+    0x77,
+    0x1F,
+    0x4E,
+    0x3D,
+    0x4F,
+    0x47,
+  )
+
+  val data = RegInit(0.U(8.W))
+  val patterns = VecInit(tbl.map((x: Int) => ~(x.U(8.W) << 1)))
+  io.out.seg(0) := patterns(data(3, 0))
+  io.out.seg(1) := patterns(data(7, 4))
+
+  when (cpu.io.outValid) {
+    data := cpu.io.out
+  }
 }
---
+++ npc/playground/src/sCPU.scala
@@ -0,0 +1,109 @@
+package top
+
+import chisel3._
+import chisel3.util.experimental.BoringUtils
+
+import scala.collection.mutable
+
+class sCPU(program: Seq[Byte]) extends Module {
+  val io = IO(new Bundle {
+    val out      = Output(UInt(8.W))
+    val outValid = Output(Bool())
+  })
+
+  val rom  = VecInit(program.toIndexedSeq.map(_.S(32.W).asUInt))
+  val regs = RegInit(VecInit(Seq.fill(4)(0.U(8.W))))
+  val pc   = RegInit(0.U(8.W))
+
+  val output      = RegInit(0.U(8.W))
+  val outputValid = RegInit(false.B)
+  io.out      := output
+  io.outValid := outputValid
+
+  // decode
+  val inst = rom(pc)
+  val op   = inst(7, 6)
+  val rd   = inst(5, 4)
+  val rs1  = inst(3, 2)
+  val rs2  = inst(1, 0)
+
+  import sCPU._
+  when(op === ADD.U) {
+    regs(rd) := regs(rs1) + regs(rs2)
+  }
+
+  when(op === LI.U) {
+    regs(rd) := inst(3, 0)
+  }
+
+  when(op === OUT.U) {
+    output      := regs(rs1)
+    outputValid := true.B
+  }
+
+  val brTaken = op === BNER0.U && regs(0) =/= regs(rs2)
+  pc := Mux(brTaken, inst(5, 2), pc + 1.U)
+
+  BoringUtils.bore(pc)
+}
+
+object sCPU {
+  val ADD   = 0
+  val LI    = 2
+  val BNER0 = 3
+  val OUT   = 1
+
+  //  7  6 5  4 3   2 1   0
+  // +----+----+-----+-----+
+  // | 00 | rd | rs1 | rs2 | R[rd]=R[rs1]+R[rs2]                add
+  // +----+----+-----+-----+
+  // | 10 | rd |    imm    | R[rd]=imm                          li
+  // +----+----+-----+-----+
+  // | 11 |   addr   | rs2 | if (R[0]!=R[rs2]) PC=addr          bner0
+  // +----+----------+-----+
+  // | 01 |    | rs1 |     | output rs1                         out
+  // +----+----------+-----+
+  def assemble(code: String): Seq[Byte] = {
+    val lines  = code.split("\n").filter(_.nonEmpty)
+    val labels = mutable.Map[String, Int]()
+    val insts  = mutable.Buffer[String]()
+    var addr   = 0
+    for (raw_line <- lines) {
+      val line = raw_line.split("#")(0)
+      if (line.contains(":"))
+        labels += (line.split(":")(0) -> addr)
+      else {
+        insts += line
+        addr += 1
+      }
+    }
+
+    def operand(name: String, shift: Int = 0) = {
+      if (name.startsWith("r"))
+        name.substring(1).toInt << shift
+      else
+        throw new Exception(s"Invalid operand: $name")
+    }
+
+    def label(name: String, shift: Int = 0) = {
+      if (!labels.contains(name))
+        throw new Exception(s"Label $name not found")
+      labels(name) << shift
+    }
+
+    insts.map { inst =>
+      {
+        inst.trim.split("[ ,]+") match {
+          case Array("add", rd, rs1, rs2) =>
+            (ADD << 6) | operand(rd, 4) | operand(rs1, 2) | operand(rs2)
+          case Array("li", rd, imm)       =>
+            (LI << 6) | operand(rd, 4) | imm.toInt
+          case Array("bner0", rs2, addr)  =>
+            (BNER0 << 6) | label(addr, 2) | operand(rs2)
+          case Array("out", rs1)          =>
+            (OUT << 6) | operand(rs1, 2)
+        }
+      }.toByte
+    }.toSeq
+  }
+}
