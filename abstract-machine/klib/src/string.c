ae71f52e35d19e1d88c2f8e27266ee2118b04841
tracer-ysyx <tracer@ysyx.org>
2025-10-05 22:30:41 +0800

    > compile NEMU ysyx_22040000 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  22:30:41 up 12:43,  1 user,  load average: 2.34, 2.38, 2.36

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -37,12 +37,12 @@ char *strcat(char *dst, const char *src) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("t");
-  while (*s1 && (*s1 == *s2)) {
-    s1++;
-    s2++;
-  }
-  return *(const unsigned char*)s1 - *(const unsigned char*)s2;
+  panic("Not implemented");
+  // while (*s1 && (*s1 == *s2)) {
+  //   s1++;
+  //   s2++;
+  // }
+  // return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
