ab45bde432864b890171945b6b6fae319f81fe86
tracer-ysyx <tracer@ysyx.org>
2025-10-05 22:28:22 +0800

    > compile NEMU ysyx_22040000 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  22:28:22 up 12:41,  1 user,  load average: 2.23, 2.21, 2.30

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -41,7 +41,7 @@ int strcmp(const char *s1, const char *s2) {
     s1++;
     s2++;
   }
-  return (int)(*s1) - (int)(*s2);
+  return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
