371663fcfeb857d10603063c05e9cbf7bdee019b
tracer-ysyx <tracer@ysyx.org>
2025-10-06 22:33:27 +0800

    > compile NEMU ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  22:33:27 up  8:45,  1 user,  load average: 2.74, 2.21, 2.23

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -75,8 +75,11 @@ void paddr_write(paddr_t addr, int len, word_t data) {
     return;
   }
 
-  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data));
-  IFDEF(CONFIG_DEVICE && CONFIG_MTRACE, Log("paddr_write: -> mmio_write"));
+#ifdef CONFIG_DEVICE
+  mmio_write(addr, len, data)
+  IFDEF(CONFIG_MTRACE, Log("paddr_write: -> mmio_write"));
+  return;
+#endif
 
   out_of_bound(addr);
 }
