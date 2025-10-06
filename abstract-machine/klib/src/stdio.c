e8a3c8d6d332384fe5d6963606260b07f0375992
tracer-ysyx <tracer@ysyx.org>
2025-10-06 14:52:51 +0800

    > compile NEMU ysyx_22040000 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  14:52:51 up  1:04,  1 user,  load average: 3.08, 2.57, 2.10

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,74 +14,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  char *p = out;
-  const char *f = fmt;
-  va_list ap;
-  va_start(ap, fmt);
-
-  while (*f) {
-    if (*f != '%') {
-      *p++ = *f++;
-      continue;
-    }
-
-    f++; // eat '%'
-    if (*f == '\0')
-      break;
-
-    // handle %%
-    if (*f == '%') {
-      *p++ = '%';
-      f++;
-      continue;
-    }
-
-    if (*f == 's') {
-      const char *s = va_arg(ap, const char *);
-      if (!s)
-        s = "(null)";
-      while (*s)
-        *p++ = *s++;
-      f++;
-    } else if (*f == 'd') {
-      int v = va_arg(ap, int);
-      long long val = v; // promote to signed long long for safe negation
-      unsigned long long u;
-      int neg = 0;
-      if (val < 0) {
-        neg = 1;
-        u = (unsigned long long)(-val);
-      } else {
-        u = (unsigned long long)val;
-      }
-
-      // convert u to digits in temp buffer (reverse)
-      char buf[32];
-      int bi = 0;
-      if (u == 0) {
-        buf[bi++] = '0';
-      } else {
-        while (u > 0) {
-          buf[bi++] = '0' + (int)(u % 10);
-          u /= 10;
-        }
-      }
-      if (neg) *p++ = '-';
-      // write digits in correct order
-      while (bi > 0) {
-        *p++ = buf[--bi];
-      }
-      f++;
-    } else {
-      // unsupported format: treat '%' + char as literal (like many simple implementations)
-      *p++ = '%';
-      *p++ = *f++;
-    }
-  }
-
-  *p = '\0';
-  va_end(ap);
-  return (int)(p - out);
+return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
