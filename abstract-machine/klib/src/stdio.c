51303fdd4a96bdd0c5cb05313f19e92e6b3d43ee
tracer-ysyx <tracer@ysyx.org>
2025-10-06 14:58:51 +0800

    > compile NEMU ysyx_22040000 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  14:58:51 up  1:10,  1 user,  load average: 3.94, 3.31, 2.58

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,8 +13,73 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
+// ISO C11:
+// http://port70.net/~nsz/c/c11/n1570.html#7.21.6.6
 int sprintf(char *out, const char *fmt, ...) {
-return 0;
+  char *p = out;
+  const char *f = fmt;
+  va_list ap;
+  va_start(ap, fmt);
+
+  while (*f) {
+    if (*f != '%') {
+      *p++ = *f++;
+      continue;
+    }
+
+    f++; // eat '%'
+    if (*f == '\0')
+      break;
+
+    // handle %%
+    if (*f == '%') {
+      *p++ = '%';
+      f++;
+      continue;
+    }
+
+    if (*f == 's') {
+      const char *s = va_arg(ap, const char *);
+      while (*s)
+        *p++ = *s++;
+      f++;
+    } else if (*f == 'd') {
+      int v = va_arg(ap, int);
+      // long long to avoid overflow when negation
+      long long val = v;
+      unsigned long long u;
+      int neg = 0;
+      if (val < 0) {
+        neg = 1;
+        u = (unsigned long long)(-val);
+      } else {
+        u = (unsigned long long)val;
+      }
+
+      // Reverse buffer
+      char buf[32];
+      int bi = 0;
+      if (u == 0)
+        buf[bi++] = '0';
+      else {
+        while (u > 0) {
+          buf[bi++] = '0' + (int)(u % 10);
+          u /= 10;
+        }
+      }
+      if (neg)
+        *p++ = '-';
+      while (bi > 0)
+        *p++ = buf[--bi];
+
+      f++;
+    } else
+      panic("unsupported format.");
+  }
+
+  *p = '\0';
+  va_end(ap);
+  return (int)(p - out);
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
