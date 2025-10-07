3d86ca3a396c8bd597c55ed6842447350e8d1dad
tracer-ysyx <tracer@ysyx.org>
2025-10-07 23:42:42 +0800

    > compile NEMU ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  23:42:42 up 57 min,  1 user,  load average: 3.44, 2.94, 2.74

--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -18,7 +18,7 @@
 #include <SDL2/SDL.h>
 
 enum {
-  reg_freq,
+  reg_freq = 0,
   reg_channels,
   reg_samples,
   reg_sbuf_size,
@@ -30,12 +30,52 @@ enum {
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
 
+static void sdl_audio_callback(void *userdata, uint8_t *stream, int len) {
+  int write_len = (len > audio_base[reg_count]) ? audio_base[reg_count] : len;
+  memcpy(stream, sbuf, write_len);
+  memset(stream + write_len, 0, len - write_len);
+  audio_base[reg_count] -= write_len;
+}
+
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+  switch (offset) {
+  case reg_freq << 4:
+  case reg_channels << 4:
+  case reg_samples << 4:
+  case reg_count << 4:
+    // pass
+    break;
+
+  case reg_sbuf_size << 4:
+    Assert(!is_write, "write to read-only register 'sbuf_size'");
+    break;
+
+  case reg_init << 4: {
+    SDL_AudioSpec s = {};
+
+    s.format = AUDIO_S16SYS;  // 假设系统中音频数据的格式总是使用16位有符号数来表示
+    s.userdata = NULL;        // 不使用
+    s.freq = audio_base[reg_freq];
+    s.channels = audio_base[reg_channels];
+    s.samples = audio_base[reg_samples];
+    s.callback = sdl_audio_callback;
+
+    SDL_InitSubSystem(SDL_INIT_AUDIO);
+    SDL_OpenAudio(&s, NULL);
+    SDL_PauseAudio(0);
+  }
+    break;
+  default:
+    panic("audio: unknown offset = 0x%x", offset);
+  }
 }
 
 void init_audio() {
   uint32_t space_size = sizeof(uint32_t) * nr_reg;
   audio_base = (uint32_t *)new_space(space_size);
+
+  audio_base[reg_sbuf_size] = CONFIG_SB_SIZE;
+
 #ifdef CONFIG_HAS_PORT_IO
   add_pio_map ("audio", CONFIG_AUDIO_CTL_PORT, audio_base, space_size, audio_io_handler);
 #else
