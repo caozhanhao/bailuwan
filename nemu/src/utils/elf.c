4275e8b523182c9a65e54c1f49cd065698ade49f
caozhanhao <cao2013zh@163.com>
2025-10-07 14:11:48 +0800

    nemu: ftrace

--- nemu/src/utils/elf.c
+++ nemu/src/utils/elf.c
@@ -13,59 +13,120 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
-#include <dlfcn.h>
-#include <capstone/capstone.h>
-#include <common.h>
+#include "debug.h"
 
-static size_t (*cs_disasm_dl)(csh handle, const uint8_t *code,
-    size_t code_size, uint64_t address, size_t count, cs_insn **insn);
-static void (*cs_free_dl)(cs_insn *insn, size_t count);
+#include <elf.h>
+#include <stdio.h>
 
-static csh handle;
+typedef struct ftrace_sym_t {
+  uint32_t addr;
+  uint32_t size;
+  char name[128];
+} ftrace_sym_t;
 
-void init_disasm() {
-  void *dl_handle;
-  dl_handle = dlopen("tools/capstone/repo/libcapstone.so.5", RTLD_LAZY);
-  assert(dl_handle);
+#define FTRACE_TABLE_MAXSIZE 1024
+ftrace_sym_t ftrace_table[FTRACE_TABLE_MAXSIZE];
+size_t ftrace_size = 0;
 
-  cs_err (*cs_open_dl)(cs_arch arch, cs_mode mode, csh *handle) = NULL;
-  cs_open_dl = dlsym(dl_handle, "cs_open");
-  assert(cs_open_dl);
-
-  cs_disasm_dl = dlsym(dl_handle, "cs_disasm");
-  assert(cs_disasm_dl);
-
-  cs_free_dl = dlsym(dl_handle, "cs_free");
-  assert(cs_free_dl);
-
-  cs_arch arch = MUXDEF(CONFIG_ISA_x86,      CS_ARCH_X86,
-                   MUXDEF(CONFIG_ISA_mips32, CS_ARCH_MIPS,
-                   MUXDEF(CONFIG_ISA_riscv,  CS_ARCH_RISCV,
-                   MUXDEF(CONFIG_ISA_loongarch32r,  CS_ARCH_LOONGARCH, -1))));
-  cs_mode mode = MUXDEF(CONFIG_ISA_x86,      CS_MODE_32,
-                   MUXDEF(CONFIG_ISA_mips32, CS_MODE_MIPS32,
-                   MUXDEF(CONFIG_ISA_riscv,  MUXDEF(CONFIG_ISA64, CS_MODE_RISCV64, CS_MODE_RISCV32) | CS_MODE_RISCVC,
-                   MUXDEF(CONFIG_ISA_loongarch32r,  CS_MODE_LOONGARCH32, -1))));
-	int ret = cs_open_dl(arch, mode, &handle);
-  assert(ret == CS_ERR_OK);
-
-#ifdef CONFIG_ISA_x86
-  cs_err (*cs_option_dl)(csh handle, cs_opt_type type, size_t value) = NULL;
-  cs_option_dl = dlsym(dl_handle, "cs_option");
-  assert(cs_option_dl);
-
-  ret = cs_option_dl(handle, CS_OPT_SYNTAX, CS_OPT_SYNTAX_ATT);
-  assert(ret == CS_ERR_OK);
-#endif
+static void *read_chunk(FILE *fp, size_t off, size_t sz) {
+  void *buf = malloc(sz);
+  if (fseek(fp, (long)off, SEEK_SET) != 0) {
+    free(buf);
+    panic("fseek fail");
+  }
+  size_t r = fread(buf, 1, sz, fp);
+  Assert(r == sz, "read fail");
+  return buf;
 }
 
-void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
-  cs_insn *insn;
-  size_t count = cs_disasm_dl(handle, code, nbyte, pc, 0, &insn);
-  assert(count == 1);
-  int ret = snprintf(str, size, "%s", insn->mnemonic);
-  if (insn->op_str[0] != '\0') {
-    snprintf(str + ret, size - ret, "\t%s", insn->op_str);
+static void free_chunk(void *buf) {
+  free(buf);
+}
+
+void ftrace_table_push(uint32_t addr, uint32_t size, const char *name) {
+  Assert(ftrace_size < FTRACE_TABLE_MAXSIZE, "ftrace table is full");
+  ftrace_table[ftrace_size].addr = addr;
+  ftrace_table[ftrace_size].size = size;
+
+  Assert(name != nullptr, "name is null");
+  Assert(strlen(name) < sizeof(ftrace_table[ftrace_size].name), "name is too long");
+
+  strncpy(ftrace_table[ftrace_size].name, name, sizeof(ftrace_table[ftrace_size].name));
+  ftrace_size++;
+  Log("ftrace: %s @ 0x%x", name, addr);
+}
+
+const char* ftrace_search(uint32_t pc) {
+  for (size_t i = 0; i < ftrace_size; i++) {
+    if (pc >= ftrace_table[i].addr && pc < ftrace_table[i].addr + ftrace_table[i].size)
+      return ftrace_table[i].name;
   }
-  cs_free_dl(insn, count);
+  return NULL;
+}
+
+void init_ftrace(const char *file) {
+  if (file == NULL) {
+    Log("ftrace: no file specified");
+    return;
+  }
+
+  FILE *fp = fopen(file, "rb");
+  Assert(fp != NULL, "Can not open %s", file);
+  Log("Reading ELF: %s", file);
+
+  unsigned char e_ident[EI_NIDENT];
+  size_t r = fread(e_ident, 1, EI_NIDENT, fp);
+  Assert(r == EI_NIDENT, "read elf ident fail");
+  Assert(e_ident[EI_MAG0] == ELFMAG0 && e_ident[EI_MAG1] == ELFMAG1 && e_ident[EI_MAG2] == ELFMAG2 &&
+             e_ident[EI_MAG3] == ELFMAG3,
+         "not an ELF file");
+
+  Assert(e_ident[EI_CLASS] == ELFCLASS32, "Unsupported ELF class");
+  Assert(e_ident[EI_DATA] == ELFDATA2LSB, "Unsupported ELF endian");
+
+  // rewind to beginning and parse proper header
+  fseek(fp, 0, SEEK_SET);
+
+  Elf32_Ehdr eh;
+  r = fread(&eh, 1, sizeof(eh), fp);
+  Assert(r == sizeof(eh), "read ehdr failed");
+
+  // TODO
+  Assert(eh.e_shnum > 0, "todo: SHN_LORESERVE");
+
+  // read all section headers
+  Elf32_Shdr *shdrs = (Elf32_Shdr *)read_chunk(fp, eh.e_shoff, eh.e_shentsize * eh.e_shnum);
+
+  // find SHT_SYMTAB / SHT_DYNSYM
+  for (int i = 0; i < eh.e_shnum; i++) {
+    uint32_t stype = shdrs[i].sh_type;
+    if (stype == SHT_SYMTAB || stype == SHT_DYNSYM) {
+      uint32_t sz_ent = shdrs[i].sh_entsize;
+      uint32_t num_ents = shdrs[i].sh_size / sz_ent;
+      void *section = read_chunk(fp, shdrs[i].sh_offset, shdrs[i].sh_size);
+
+      // linked string table index
+      uint32_t stridx = shdrs[i].sh_link;
+      uint32_t str_off = shdrs[stridx].sh_offset;
+      uint32_t str_size = shdrs[stridx].sh_size;
+      char *strtab = (char *)read_chunk(fp, str_off, str_size);
+
+      for (uint32_t si = 0; si < num_ents; si++) {
+        Elf32_Sym sym;
+        memcpy(&sym, (char *)section + si * sz_ent, sizeof(sym));
+        unsigned char st_type = ELF32_ST_TYPE(sym.st_info);
+        if (st_type == STT_FUNC && sym.st_shndx != SHN_UNDEF) {
+          const char *name = strtab + sym.st_name;
+          ftrace_table_push(sym.st_value, sym.st_size, name);
+        }
+      }
+      free_chunk(section);
+      free_chunk(strtab);
+    }
+  }
+
+  free_chunk(shdrs);
+  fclose(fp);
+
+  Log("ftrace: loaded %zu function symbols", ftrace_size);
 }
