600f42bad6fdf6c6314d0df2a81d4f1fa58c3398
tracer-ysyx <tracer@ysyx.org>
2025-10-08 00:14:35 +0800

    > compile NEMU ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  00:14:35 up  1:29,  1 user,  load average: 2.97, 2.38, 2.25

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -25,7 +25,7 @@ void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = inl(AUDIO_RPTR_ADDR);
+  stat->count = inl(AUDIO_WPTR_ADDR) - inl(AUDIO_RPTR_ADDR);
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
