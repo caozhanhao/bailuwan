80ec0634ab12a88deeeacb56417d6fd76ec3762a
tracer-ysyx <tracer@ysyx.org>
2025-10-07 22:25:36 +0800

    > compile NEMU ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  22:25:36 up 12:53,  1 user,  load average: 2.08, 2.23, 2.23

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,6 +5,6 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t key = inl(KBD_ADDR);
-  kbd->keydown = key != AM_KEY_NONE;
-  kbd->keycode = key;
+  kbd->keydown = key & KEYDOWN_MASK;
+  kbd->keycode = key & ~KEYDOWN_MASK;
 }
