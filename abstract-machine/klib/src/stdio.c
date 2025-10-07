ff8253f5b898ba6a5d3ed0ec4a13e2d49fea1243
tracer-ysyx <tracer@ysyx.org>
2025-10-07 17:09:13 +0800

    > compile NEMU ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  17:09:13 up  7:37,  1 user,  load average: 3.17, 2.49, 2.46

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,105 +5,145 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  // FIXME
-  char buf[1024];
-  va_list ap;
+int printf(const char* fmt, ...)
+{
+    // FIXME
+#define BUF_SZ 1024
+    char buf[BUF_SZ];
+    va_list ap;
+    va_start(ap, fmt);
+    int needed = vsnprintf(buf, BUF_SZ, fmt, ap);
+    va_end(ap);
 
-  int ret;
+    if (needed < 0) return needed;
 
-  va_start(ap, fmt);
-  ret = sprintf(buf, fmt, ap);
-  va_end(ap);
+    int to_write = 0;
+    if (needed >= BUF_SZ)
+        to_write = BUF_SZ - 1;
+    else
+        to_write = needed;
 
-  if (ret >= 0) {
-    for (int i = 0; i < ret; i++)
-    {
-      putch(buf[i]);
-    }
-  }
-  return ret;
+    for (int i = 0; i < to_write; i++)
+        putch(buf[i]);
+
+#undef BUF_SZ
+    return to_write;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+int vsprintf(char* out, const char* fmt, va_list ap)
+{
+    return vsnprintf(out, (size_t)SIZE_MAX, fmt, ap);
 }
 
 // ISO C11:
 // http://port70.net/~nsz/c/c11/n1570.html#7.21.6.6
-int sprintf(char *out, const char *fmt, ...) {
-  char *p = out;
-  const char *f = fmt;
-  va_list ap;
-  va_start(ap, fmt);
+int sprintf(char* out, const char* fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    int r = vsnprintf(out, (size_t)SIZE_MAX, fmt, ap);
+    va_end(ap);
+    return r;
+}
 
-  while (*f) {
-    if (*f != '%') {
-      *p++ = *f++;
-      continue;
-    }
+int snprintf(char* out, size_t n, const char* fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    int r = vsnprintf(out, n, fmt, ap);
+    va_end(ap);
+    return r;
+}
 
-    f++; // eat '%'
-    if (*f == '\0')
-      break;
+int vsnprintf(char* out, size_t n, const char* fmt, va_list ap)
+{
+    size_t pos = 0; // written
+    size_t needed = 0;
+    size_t cap = (n > 0) ? (n - 1) : 0;
 
-    // handle %%
-    if (*f == '%') {
-      *p++ = '%';
-      f++;
-      continue;
-    }
+    const char* f = fmt;
 
-    if (*f == 's') {
-      const char *s = va_arg(ap, const char *);
-      while (*s)
-        *p++ = *s++;
-      f++;
-    } else if (*f == 'd') {
-      int v = va_arg(ap, int);
-      // long long to avoid overflow when negation
-      long long val = v;
-      unsigned long long u;
-      int neg = 0;
-      if (val < 0) {
-        neg = 1;
-        u = (unsigned long long)(-val);
-      } else {
-        u = (unsigned long long)val;
-      }
+#define WRITE_CHAR(c) do { \
+        if (pos < cap) out[pos++] = (c); \
+        needed++; \
+    } while (0)
 
-      // Reverse buffer
-      char buf[32];
-      int bi = 0;
-      if (u == 0)
-        buf[bi++] = '0';
-      else {
-        while (u > 0) {
-          buf[bi++] = '0' + (int)(u % 10);
-          u /= 10;
+    while (*f)
+    {
+        if (*f != '%')
+        {
+            WRITE_CHAR(*f++);
+            continue;
         }
-      }
-      if (neg)
-        *p++ = '-';
-      while (bi > 0)
-        *p++ = buf[--bi];
 
-      f++;
-    } else
-      panic("unsupported format.");
-  }
+        f++;
+        if (*f == '\0')
+            break;
 
-  *p = '\0';
-  va_end(ap);
-  return (int)(p - out);
-}
+        // %% -> %
+        if (*f == '%')
+        {
+            WRITE_CHAR('%');
+            f++;
+            continue;
+        }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
-}
+        if (*f == 's')
+        {
+            const char* s = va_arg(ap, const char *);
+            if (!s)
+                s = "(null)";
+            while (*s)
+                WRITE_CHAR(*s++);
+            f++;
+        }
+        else if (*f == 'd')
+        {
+            int v = va_arg(ap, int);
+            long long val = (long long)v;
+            unsigned long long u;
+            if (val < 0)
+            {
+                WRITE_CHAR('-');
+                u = (unsigned long long)(-val);
+            }
+            else
+            {
+                u = (unsigned long long)val;
+            }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+            char buf[32];
+            int bi = 0;
+            if (u == 0)
+            {
+                buf[bi++] = '0';
+            }
+            else
+            {
+                while (u > 0 && bi < (int)sizeof(buf))
+                {
+                    buf[bi++] = (char)('0' + (u % 10));
+                    u /= 10;
+                }
+            }
+            while (bi > 0)
+                WRITE_CHAR(buf[--bi]);
+
+            f++;
+        }
+        else
+        {
+            WRITE_CHAR('%');
+            WRITE_CHAR(*f ? *f : '\0');
+            if (*f) f++;
+        }
+    }
+
+    if (n > 0)
+        out[pos] = '\0';
+
+    return (int)needed;
+#undef WRITE_CHAR
 }
 
 #endif
