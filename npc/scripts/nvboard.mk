78022f9f8864b2536b610f3a59bb0e74fa5bd75b
tracer-ysyx <tracer@ysyx.org>
2025-10-09 19:55:44 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  19:55:44 up  1:45,  1 user,  load average: 2.13, 2.83, 3.07

--- npc/scripts/nvboard.mk
+++ npc/scripts/nvboard.mk
@@ -4,8 +4,8 @@ $(SRC_AUTO_BIND): $(NXDC_FILES)
 	python3 $(NVBOARD_HOME)/scripts/auto_pin_bind.py $^ $@
 
 CSRCS += $(SRC_AUTO_BIND)
+LINKAGE += $(NVBOARD_ARCHIVE)
 
 # rules for NVBoard
 include $(NVBOARD_HOME)/scripts/nvboard.mk
 
-LINKAGE += $(NVBOARD_ARCHIVE)
