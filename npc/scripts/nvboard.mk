33496aecaa160b900ddca4e1cd5a015ae77dc765
tracer-ysyx <tracer@ysyx.org>
2025-10-09 20:12:15 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  20:12:15 up  2:01,  1 user,  load average: 3.56, 2.66, 2.65

--- npc/scripts/nvboard.mk
+++ npc/scripts/nvboard.mk
@@ -4,7 +4,9 @@ SRC_AUTO_BIND = $(abspath $(BUILD_DIR)/auto_bind.cpp)
 $(SRC_AUTO_BIND): $(NXDC_FILES)
 	python3 $(NVBOARD_HOME)/scripts/auto_pin_bind.py $^ $@
 
+CSRCS += $(shell find $(abspath ./csrc/nvboard) -name "*.c" -or -name "*.cc" -or -name "*.cpp")
 CSRCS += $(SRC_AUTO_BIND)
+
 LINKAGE += $(NVBOARD_ARCHIVE)
 
 # rules for NVBoard
