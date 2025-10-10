db988f52bb11dead44f725f07173964d9936caf5
tracer-ysyx <tracer@ysyx.org>
2025-10-10 16:44:50 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  16:44:50 up  2:17,  1 user,  load average: 2.47, 2.71, 2.54

--- npc/csrc/debug/main.cpp
+++ npc/csrc/debug/main.cpp
@@ -1,6 +1,18 @@
 #include <VTop.h>
+#include "macro.h"
+
+#ifdef TRACE_FST
+#include "verilated_fst_c.h"
+#define TRACE_HANDLE_TYPE VerilatedFstC
+#endif
+
+#ifdef TRACE_VCD
+#include "verilated_vcd_c.h"
+#define TRACE_HANDLE_TYPE VerilatedVcdC
+#endif
 
 static TOP_NAME dut;
+static TRACE_HANDLE *trace_handle;
 
 static void single_cycle() {
   dut.clock = 0; dut.eval();
@@ -14,6 +26,11 @@ static void reset(int n) {
 }
 
 int main() {
+  trace_handle = new TRACE_HANDLE_TYPE;
+  Verilated::traceEverOn(true);
+  dut->trace(trace_handle, 0);
+  trace_handle->open(TRACE_FILENAME);
+
   reset(10);
 
   while(1) {
