1057bdd78a64258a721a8b0aa4a4fca280c0c4a2
tracer-ysyx <tracer@ysyx.org>
2025-10-10 23:02:39 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  23:02:39 up  4:52,  1 user,  load average: 1.83, 2.06, 1.88

--- npc/csrc/nvboard/main.cpp
+++ npc/csrc/nvboard/main.cpp
@@ -5,8 +5,35 @@ static TOP_NAME dut;
 
 void nvboard_bind_all_pins(TOP_NAME* top);
 
+
+// 00000000 <_start>:
+//      0:	01400513          	addi	a0,zero,20
+//      4:	010000e7          	jalr	ra,16(zero) # 10 <fun>
+//      8:	00c000e7          	jalr	ra,12(zero) # c <halt>
+//
+//   0000000c <halt>:
+//      c:	00c00067          	jalr	zero,12(zero) # c <halt>
+//
+//   00000010 <fun>:
+//     10:	00a50513          	addi	a0,a0,10
+//     14:	00008067          	jalr	zero,0(ra)
+uint32_t inst_mem[1024] = {
+    0x01400513, // addi a0, zero, 20
+    0x010000e7, // jalr ra, 16(zero)
+    0x00c000e7, // jalr ra, 12(zero)
+    0x00c000e7, // jalr	zero,12(zero) # c <halt>
+    0x00a50513, // addi a0, a0, 10
+    0x00008067, // jalr zero,0(ra)
+};
+static uint32_t pmem_read(uint32_t addr) {
+  return inst_mem[addr / 4];
+}
+
 static void single_cycle() {
   dut.clock = 0; dut.eval();
+
+  dut.io_mem_inst = pmem_read(dut.io_mem_pc);
+
   dut.clock = 1; dut.eval();
 }
 
