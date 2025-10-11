583be3047d2c4d1cb93cbee7836c405e0631f2b3
tracer-ysyx <tracer@ysyx.org>
2025-10-11 13:20:59 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  13:20:59 up  4:21,  1 user,  load average: 2.46, 2.16, 2.23

--- npc/csrc/debug/main.cpp
+++ npc/csrc/debug/main.cpp
@@ -4,26 +4,6 @@
 #include "macro.h"
 #include "common.h"
 
-// 00000000 <_start>:
-//      0:	01400513          	addi	a0,zero,20
-//      4:	010000e7          	jalr	ra,16(zero) # 10 <fun>
-//      8:	00c000e7          	jalr	ra,12(zero) # c <halt>
-//
-//   0000000c <halt>:
-//      c:  00100073            ebreak
-//
-//   00000010 <fun>:
-//     10:	00a50513          	addi	a0,a0,10
-//     14:	00008067          	jalr	zero,0(ra)
-uint32_t inst_mem[1024] = {
-    0x01400513, // addi a0, zero, 20
-    0x010000e7, // jalr ra, 16(zero)
-    0x00c000e7, // jalr ra, 12(zero)
-    0x00100073, // ebreak
-    0x00a50513, // addi a0, a0, 10
-    0x00008067, // jalr zero,0(ra)
-};
-
 static uint64_t sim_time = 0;
 
 static void single_cycle()
