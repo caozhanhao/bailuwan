896b6b948cb2ab150f73c0d163c22e1f315bf036
tracer-ysyx <tracer@ysyx.org>
2025-10-11 22:27:30 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  22:27:30 up  1:08,  1 user,  load average: 3.27, 2.53, 2.39

--- npc/npc/src/core/LSU.scala
+++ npc/npc/src/core/LSU.scala
@@ -7,56 +7,74 @@ import utils.Utils._
 
 class LSU extends Module {
   val io = IO(new Bundle {
-    val lsu_op = Input(UInt(LSUOp.WIDTH))
-    val addr = Input(UInt(32.W))
+    val lsu_op     = Input(UInt(LSUOp.WIDTH))
+    val addr       = Input(UInt(32.W))
     val write_data = Input(UInt(32.W))
-    val read_data = Output(UInt(32.W))
+    val read_data  = Output(UInt(32.W))
   })
 
-  val write_enable = MuxLookup(io.lsu_op, false.B)(Seq(
-    LSUOp.SB -> true.B,
-    LSUOp.SH -> true.B,
-    LSUOp.SW -> true.B
-  ))
+  val write_enable = MuxLookup(io.lsu_op, false.B)(
+    Seq(
+      LSUOp.SB -> true.B,
+      LSUOp.SH -> true.B,
+      LSUOp.SW -> true.B
+    )
+  )
 
-  val write_mask = MuxLookup(io.lsu_op, 0.U(8.W))(Seq(
-    LSUOp.SB -> 0x1.U(8.W),
-    LSUOp.SH -> 0x3.U(8.W),
-    LSUOp.SW -> 0xf.U(8.W)
-  ))
+  val write_mask = MuxLookup(io.lsu_op, 0.U(8.W))(
+    Seq(
+      LSUOp.SB -> 0x1.U(8.W),
+      LSUOp.SH -> 0x3.U(8.W),
+      LSUOp.SW -> 0xf.U(8.W)
+    )
+  )
 
-  val read_enable = io.lsu_op =/= LSUOp.None && !write_enable
+  // val read_enable = io.lsu_op =/= LSUOp.None && !write_enable
+  val read_enable = MuxLookup(io.lsu_op, false.B)(
+    Seq(
+      LSUOp.LB  -> true.B,
+      LSUOp.LH  -> true.B,
+      LSUOp.LW  -> true.B,
+      LSUOp.LBU -> true.B,
+      LSUOp.LHU -> true.B
+    )
+  )
 
   val mem = Module(new DPICMem())
 
-  mem.io.addr := io.addr
-  mem.io.read_enable := read_enable
+  mem.io.addr         := io.addr
+  mem.io.read_enable  := read_enable
   mem.io.write_enable := write_enable
-  mem.io.write_mask := write_mask
-  mem.io.write_data := io.write_data
+  mem.io.write_mask   := write_mask
+  mem.io.write_data   := io.write_data
 
   val data_out = mem.io.data_out
 
-  val lb_sel = MuxLookup(io.addr(1, 0), 0.U(8.W))(Seq(
-    0.U -> data_out(7, 0),
-    1.U -> data_out(15, 8),
-    2.U -> data_out(23, 16),
-    3.U -> data_out(31, 24)
-  ))
+  val lb_sel = MuxLookup(io.addr(1, 0), 0.U(8.W))(
+    Seq(
+      0.U -> data_out(7, 0),
+      1.U -> data_out(15, 8),
+      2.U -> data_out(23, 16),
+      3.U -> data_out(31, 24)
+    )
+  )
 
-  val lh_sel = MuxLookup(io.addr(1, 0), 0.U(16.W))(Seq(
-    0.U -> data_out(15, 0),
-    2.U -> data_out(31, 16)
-  ))
+  val lh_sel = MuxLookup(io.addr(1, 0), 0.U(16.W))(
+    Seq(
+      0.U -> data_out(15, 0),
+      2.U -> data_out(31, 16)
+    )
+  )
 
-  val selected_data = MuxLookup(io.lsu_op, 0.U(32.W))(Seq(
-    LSUOp.LB  -> sign_extend(lb_sel, 32),
-    LSUOp.LH  -> sign_extend(lh_sel, 32),
-    LSUOp.LW  -> data_out,
-    LSUOp.LBU -> zero_extend(lb_sel, 32),
-    LSUOp.LHU -> zero_extend(lh_sel, 32)
-  ))
+  val selected_data = MuxLookup(io.lsu_op, 0.U(32.W))(
+    Seq(
+      LSUOp.LB  -> sign_extend(lb_sel, 32),
+      LSUOp.LH  -> sign_extend(lh_sel, 32),
+      LSUOp.LW  -> data_out,
+      LSUOp.LBU -> zero_extend(lb_sel, 32),
+      LSUOp.LHU -> zero_extend(lh_sel, 32)
+    )
+  )
 
   io.read_data := selected_data
 }
-
