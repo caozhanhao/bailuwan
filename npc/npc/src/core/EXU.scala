71c6f0b41bac55319775c1ff485da170c61e0884
tracer-ysyx <tracer@ysyx.org>
2025-10-11 22:15:29 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  22:15:29 up 56 min,  1 user,  load average: 3.94, 2.72, 2.45

--- npc/npc/src/core/EXU.scala
+++ npc/npc/src/core/EXU.scala
@@ -45,7 +45,7 @@ class EXU extends Module {
   lsu.io.addr := alu.io.result
   lsu.io.write_data := rs2_data
 
-  printf(cf"[EXU]: lsu_op: ${lsu.io.lsu_op}, addr: ${alu.io.result}, write_data: ${rs2_data}")
+  printf(cf"[EXU]: lsu_op: ${lsu.io.lsu_op}, addr: ${alu.io.result}, write_data: ${rs2_data}\n")
 
   // Branch
   // Default to be `pc + imm` for  beq/bne/... and jal.
