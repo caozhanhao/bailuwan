ed8fa62e1c8ac0e395ac463dc2b0189a55246155
tracer-ysyx <tracer@ysyx.org>
2025-10-12 19:41:54 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  19:41:54 up 10:12,  1 user,  load average: 4.75, 3.55, 3.44

--- npc/csrc/sdb/isa.cpp
+++ npc/csrc/sdb/isa.cpp
@@ -91,6 +91,15 @@ static int ftrace_dump(int rd, int rs1, word_t imm, char* buf, size_t buf_size)
 
         snprintf(buf, buf_size, FMT_WORD ": %*s%s [%s@" FMT_WORD "], depth=%d",
                  pc, depth * 2, "", rd == 1 ? "call" : "tail", callee, dnpc, depth);
+
+        std::string test = buf;
+        printf("\n\n\n\nDumping buf:\n");
+        for (const auto& ch : test)
+        {
+            printf("%d", ch);
+        }
+        printf("buf dumped\n\n\n\n");
+        printf("str ver: \"%s\"\n", buf);
     }
     else if (is_ret)
     {
