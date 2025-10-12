badca2c60b44d2247d96adb7ce1b699b9372a2c5
tracer-ysyx <tracer@ysyx.org>
2025-10-12 20:01:30 +0800

    > compile NEMU ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  20:01:30 up 10:32,  1 user,  load average: 2.65, 2.56, 2.68

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -13,26 +13,39 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
-#include <isa.h>
 #include <cpu/cpu.h>
 #include <difftest-def.h>
+#include <isa.h>
 #include <memory/paddr.h>
 
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  assert(0);
+  if (direction == DIFFTEST_TO_REF)
+    memcpy(guest_to_host(addr), buf, n);
+  else
+    memcpy(buf, guest_to_host(addr), n);
 }
+
+struct diff_context_t {
+  word_t gpr[32];
+  word_t pc;
+};
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-  assert(0);
+  struct diff_context_t *ctx = (struct diff_context_t *)dut;
+  if (direction == DIFFTEST_TO_REF) {
+    cpu.pc = ctx->pc;
+    for (int i = 0; i < 32; i++)
+      cpu.gpr[i] = ctx->gpr[i];
+  } else {
+    ctx->pc = cpu.pc;
+    for (int i = 0; i < 32; i++)
+      ctx->gpr[i] = cpu.gpr[i];
+  }
 }
 
-__EXPORT void difftest_exec(uint64_t n) {
-  assert(0);
-}
+__EXPORT void difftest_exec(uint64_t n) { cpu_exec(n); }
 
-__EXPORT void difftest_raise_intr(word_t NO) {
-  assert(0);
-}
+__EXPORT void difftest_raise_intr(word_t NO) { assert(0); }
 
 __EXPORT void difftest_init(int port) {
   void init_mem();
