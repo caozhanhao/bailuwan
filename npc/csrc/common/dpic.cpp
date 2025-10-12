563cfed0ae58939b95c81342f41f4644e6fb038a
tracer-ysyx <tracer@ysyx.org>
2025-10-12 13:47:20 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  13:47:20 up  4:18,  1 user,  load average: 1.89, 2.27, 2.27

--- npc/csrc/common/dpic.cpp
+++ npc/csrc/common/dpic.cpp
@@ -33,7 +33,8 @@ int pmem_read(int raddr)
         auto now = std::chrono::high_resolution_clock::now();
         auto delta = now - sim_handle.get_boot_time();
         auto sec = std::chrono::duration_cast<std::chrono::seconds>(delta);
-        return sec.count();
+        uint64_t mask = 0xFFFFu << ((uaddr - RTC_MMIO) * 8);
+        return static_cast<uint64_t>(sec.count()) & mask;
     }
     if (uaddr - RTC_MMIO >= 8 && uaddr - RTC_MMIO <= 28)
     {
