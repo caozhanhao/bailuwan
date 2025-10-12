9a8c39c6cae4b9ea434764a4e907d390d334dd44
tracer-ysyx <tracer@ysyx.org>
2025-10-12 13:55:28 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  13:55:28 up  4:26,  1 user,  load average: 2.69, 2.43, 2.34

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -8,8 +8,8 @@ void __am_timer_init() {
 #define RTC_READ(offset) *(volatile uint32_t*)(RTC_MMIO + offset)
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uint32_t lo = RTC_READ(0);
   uint32_t hi = RTC_READ(4);
+  uint32_t lo = RTC_READ(0);
   uptime->us = ((uint64_t)hi << 32) | lo;
 }
 
