4c3130165c7f4ba0d99cdd3264d5ac2d0ac2d9f7
tracer-ysyx <tracer@ysyx.org>
2025-10-12 11:55:48 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  11:55:48 up  2:26,  1 user,  load average: 2.53, 2.80, 2.75

--- npc/csrc/common/dpic.cpp
+++ npc/csrc/common/dpic.cpp
@@ -52,7 +52,7 @@ int pmem_read(int raddr)
 void pmem_write(int waddr, int wdata, char wmask)
 {
     // Serial port
-    if (waddr == 0x10000000 && wmask == 1)
+    if (waddr == 0xa00003f8 && wmask == 1)
     {
         putchar(wdata);
         fflush(stdout);
