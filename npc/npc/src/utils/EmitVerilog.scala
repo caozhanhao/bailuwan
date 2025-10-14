aa54aa1c10f018851cb09f847cb9bddd3b460f2d
tracer-ysyx <tracer@ysyx.org>
2025-10-14 21:43:39 +0800

    > compile NEMU ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  21:43:39 up  3:32,  1 user,  load average: 2.53, 2.60, 2.77

--- npc/npc/src/utils/EmitVerilog.scala
+++ npc/npc/src/utils/EmitVerilog.scala
@@ -1,7 +1,7 @@
 package utils
 
-object EmitVerilog extends App {
-  val firtoolOptions = Array(
+object CommonEmitVerilogOptions {
+  val firtool = Array(
     "--lowering-options=" + List(
       // make yosys happy
       // see https://github.com/llvm/circt/blob/main/docs/VerilogGeneration.md
@@ -10,5 +10,18 @@ object EmitVerilog extends App {
       "locationInfoStyle=wrapInAtSquareBracket"
     ).reduce(_ + "," + _)
   )
-  circt.stage.ChiselStage.emitSystemVerilogFile(new top.Top(), args, firtoolOptions)
+}
+
+object EmitVerilog extends App {
+  circt.stage.ChiselStage.emitSystemVerilogFile(new top.Top(), args, CommonEmitVerilogOptions.firtool)
+}
+
+object Emit4BitALUVerilog extends App {
+  class ALU4BitParams extends top.CoreParams {
+    override val XLEN:          Int = 4
+    override val ALUShamtWidth: Int = 1
+  }
+
+  implicit val p: ALU4BitParams = new ALU4BitParams()
+  circt.stage.ChiselStage.emitSystemVerilogFile(new core.ALU(), args, CommonEmitVerilogOptions.firtool)
 }
