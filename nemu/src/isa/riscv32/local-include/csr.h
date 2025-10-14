928c44fcc5d47081483fa61c70af25fc3696a021
caozhanhao <cao2013zh@163.com>
2025-10-15 00:10:34 +0800

    nemu: RV32 Zicsr Standard Extension.

---
+++ nemu/src/isa/riscv32/local-include/csr.h
@@ -0,0 +1,56 @@
+/***************************************************************************************
+* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+#ifndef __RISCV_CSR_H__
+#define __RISCV_CSR_H__
+
+#include <common.h>
+
+static inline int check_csr_idx(int idx) {
+  IFDEF(CONFIG_RT_CHECK, assert(idx >= 0 && idx < 4096));
+  return idx;
+}
+
+#define cpu_csr(idx) (cpu.csr[check_csr_idx(idx)])
+
+#define CSR_TABLE                                                                                                      \
+  CSR_TABLE_ENTRY(mstatus, 0x300)                                                                                      \
+  CSR_TABLE_ENTRY(mtvec, 0x305)                                                                                        \
+  CSR_TABLE_ENTRY(mepc, 0x341)                                                                                         \
+  CSR_TABLE_ENTRY(mcause, 0x342)
+
+
+// Name to Index
+// CSR_mstatus = 0x300, ...
+enum {
+#define CSR_TABLE_ENTRY(name, idx) CSR_##name = idx,
+  CSR_TABLE
+#undef CSR_TABLE_ENTRY
+};
+
+// Index to Name
+// csr_name(0x300) == "mstatus", ...
+static inline const char *csr_name(int idx) {
+#define CSR_TABLE_ENTRY(name, idx) [idx] = #name,
+  static const char *csr_names[4096] = {CSR_TABLE};
+#undef CSR_TABLE_ENTRY
+
+  const char *ret = csr_names[check_csr_idx(idx)];
+  if (ret == NULL)
+    ret = "unknown";
+  return ret;
+}
+
+#endif
--- npc/npc/src/core/ALU.scala
+++ npc/npc/src/core/ALU.scala
@@ -5,18 +5,21 @@ import chisel3.util.MuxLookup
 import constants.ALUOp
 import top.CoreParams
 
-class ALU(implicit p: CoreParams) extends Module {
+class ALU(
+  implicit p: CoreParams)
+    extends Module {
   val io = IO(new Bundle {
     val alu_op = Input(UInt(ALUOp.WIDTH))
-    val oper1 = Input(UInt(p.XLEN.W))
-    val oper2 = Input(UInt(p.XLEN.W))
+    val oper1  = Input(UInt(p.XLEN.W))
+    val oper2  = Input(UInt(p.XLEN.W))
     val result = Output(UInt(p.XLEN.W))
   })
 
-  val shamt = io.oper2(4,0)
+  val shamt = io.oper2(p.ALUShamtWidth - 1, 0)
 
   import ALUOp._
-  val result = MuxLookup(io.alu_op, 0.U)(Seq(
+  val result = MuxLookup(io.alu_op, 0.U)(
+    Seq(
       Add  -> (io.oper1 + io.oper2),
       Sub  -> (io.oper1 - io.oper2),
       Sll  -> (io.oper1 << shamt).asUInt,
@@ -26,8 +29,9 @@ class ALU(implicit p: CoreParams) extends Module {
       Srl  -> (io.oper1 >> shamt).asUInt,
       Sra  -> (io.oper1.asSInt >> shamt).asUInt,
       Or   -> (io.oper1 | io.oper2),
-      And  -> (io.oper1 & io.oper2),
-  ))
+      And  -> (io.oper1 & io.oper2)
+    )
+  )
 
   io.result := result
 }
