758bc21a6176b557043f7632ebd042564c9f11cb
tracer-ysyx <tracer@ysyx.org>
2025-10-15 23:29:30 +0800

    > compile NEMU ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  23:29:30 up  2:09,  1 user,  load average: 2.10, 1.95, 1.97

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -18,6 +18,7 @@ Context* __am_irq_handle(Context* c)
                 ev.event = EVENT_YIELD;
                 break;
             }
+        // fall through
         default:
             ev.event = EVENT_ERROR;
             break;
