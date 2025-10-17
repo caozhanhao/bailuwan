fdf8e87c7a5987f1158e453023f59cfb3c9ad190
caozhanhao <cao2013zh@163.com>
2025-10-17 17:32:58 +0800

    pa4.1: rt-thread.

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -16,6 +16,7 @@ Context* __am_irq_handle(Context* c)
             if (c->GPR1 == -1)
             {
                 ev.event = EVENT_YIELD;
+                c->mepc += 4;
                 break;
             }
         // fall through
@@ -46,7 +47,19 @@ bool cte_init(Context*(*handler)(Event, Context*))
 
 Context* kcontext(Area kstack, void (*entry)(void*), void* arg)
 {
-    return NULL;
+    Context *c = kstack.end - sizeof(Context);
+    //   uintptr_t gpr[NR_REGS], mcause, mstatus, mepc; void* pdir;
+
+    // Don't do this. It will call memset.
+    // *c = (Context) {
+    //     .mepc = (uintptr_t)entry,
+    //     .mstatus = 0x1800,
+    // };
+    c->mepc = (uintptr_t)entry;
+    c->mstatus = 0x1800;
+    c->gpr[10] = (uintptr_t)arg;
+
+    return c;
 }
 
 void yield()
