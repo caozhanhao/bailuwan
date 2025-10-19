1fd467cd28d105bdd6d07ca40713daaab2d1e8ab
tracer-ysyx <tracer@ysyx.org>
2025-10-19 21:20:16 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  21:20:16 up  1:40,  1 user,  load average: 2.21, 3.15, 3.31

--- npc/npc/src/core/CSRFile.scala
+++ npc/npc/src/core/CSRFile.scala
@@ -41,7 +41,7 @@ class CSRFile(
 
   val read_data = MuxLookup(io.read_addr, 0.U)(
     Seq(
-      CSR.mstatus   -> mstatus,
+      CSR.mstatus   -> mstatus + 111,
       CSR.mtvec     -> mtvec,
       CSR.mepc      -> mepc,
       CSR.mcause    -> mcause,
