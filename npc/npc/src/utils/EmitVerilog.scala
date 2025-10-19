368c90b962efe02722839a4584e8c18001a61a3a
tracer-ysyx <tracer@ysyx.org>
2025-10-19 13:36:19 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  13:36:19 up  3:31,  1 user,  load average: 2.25, 2.07, 2.23

--- npc/npc/src/utils/EmitVerilog.scala
+++ npc/npc/src/utils/EmitVerilog.scala
@@ -19,7 +19,6 @@ object EmitVerilog extends App {
 object Emit4BitALUVerilog extends App {
   class ALU4BitParams extends top.CoreParams {
     override val XLEN:          Int = 4
-    override val ALUShamtWidth: Int = 1
   }
 
   implicit val p: ALU4BitParams = new ALU4BitParams()
