b00c7b0943f37f2e772577644f33af44a81e4127
tracer-ysyx <tracer@ysyx.org>
2025-10-21 20:41:49 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  20:41:49 up  2:11,  1 user,  load average: 4.23, 3.84, 3.29

--- npc/csrc/common/dut_proxy.cpp
+++ npc/csrc/common/dut_proxy.cpp
@@ -165,6 +165,7 @@ uint32_t DUTMemory::read(uint32_t raddr)
         auto& cpu = sim_handle.get_cpu();
         printf("Out of bound memory access at PC = 0x%08x, raddr = 0x%08x\n", cpu.pc(), raddr);
         cpu.dump_registers(std::cerr);
+        sim_handle.cleanup();
         exit(-1);
     }
 
@@ -190,6 +191,7 @@ void DUTMemory::write(uint32_t waddr, uint32_t wdata, char wmask)
         auto& cpu = sim_handle.get_cpu();
         printf("Out of bound memory access at PC = 0x%08x, waddr = 0x%08x\n", cpu.pc(), waddr);
         cpu.dump_registers(std::cerr);
+        sim_handle.cleanup();
         exit(-1);
     }
 
