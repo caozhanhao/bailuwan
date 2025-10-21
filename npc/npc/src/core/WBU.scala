dfd6d74f491f58a35e0c6eb979c6dca35411c546
tracer-ysyx <tracer@ysyx.org>
2025-10-21 22:28:50 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  22:28:50 up  3:58,  2 users,  load average: 2.08, 2.07, 2.24

--- npc/npc/src/core/WBU.scala
+++ npc/npc/src/core/WBU.scala
@@ -51,6 +51,8 @@ class WBU(
   io.out.bits.dnpc       := dnpc
   io.regfile_out.rd_data := rd_data
   io.regfile_out.rd_we   := io.in.valid && exu_out.rd_we
+
+  // In valid -> Out Valid, thus In Ready
   io.in.ready            := io.in.valid
-  io.out.valid           := io.in.valid
+  io.out.valid           := true.B
 }
