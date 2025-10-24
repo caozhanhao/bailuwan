21b085ba0730d70965768ca82140003051534477
tracer-ysyx <tracer@ysyx.org>
2025-10-24 14:29:19 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  14:29:19 up 10 min,  1 user,  load average: 5.10, 3.42, 1.98

--- npc/npc/src/core/DPICMem.scala
+++ npc/npc/src/core/DPICMem.scala
@@ -2,6 +2,7 @@ package core
 
 import chisel3._
 import chisel3.util._
+import utils.RandomDelay
 
 class PMemReadDPICWrapper extends HasBlackBoxInline {
   val io = IO(new Bundle {
@@ -100,7 +101,7 @@ class DPICMem extends Module {
   )
 
   io.read_valid := read_valid
-  io.req_ready  := state === s_idle
+  io.req_ready  := RandomDelay(state === s_idle)
 
   val write    = Module(new PMemWriteDPICWrapper)
   val write_en = io.req_valid && io.write_enable && !reset.asBool
