d21a4ef7ab5006a4b5597c4393cb43ac513fa642
tracer-ysyx <tracer@ysyx.org>
2025-10-25 12:00:46 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  12:00:46 up  1:56,  1 user,  load average: 3.78, 2.89, 2.77

--- npc/npc/src/amba/AXI4LiteCrossBar.scala
+++ npc/npc/src/amba/AXI4LiteCrossBar.scala
@@ -32,20 +32,17 @@ class AXI4LiteCrossBar(
 
   assert(slaves_map.nonEmpty, "Crossbar for what?")
 
-  val n         = slaves_map.size
-  val idx_width = math.max(1, log2Ceil(n + 1)) // reserve 1 for err
-  val err_idx   = n.U
-  val master    = io.master
+  val n          = slaves_map.size
+  val idx_width  = math.max(1, log2Ceil(n + 1)) // reserve 1 for err
+  val real_width = math.max(1, log2Ceil(n))
+  val err_idx    = n.U
+  val master     = io.master
+  val slaves     = io.slaves
 
   val dec_err_slave = Module(new DecErrorSlave)
-  val slaves        = Vec(n + 1, IO(new AXI4Lite))
-  for (i <- 0 until n) {
-    slaves(i) <> io.slaves(i)
-  }
-  slaves(err_idx) <> dec_err_slave.io
 
   // First block all slaves
-  for (i <- 0 until n + 1) {
+  for (i <- 0 until n) {
     val s = slaves(i)
     s.ar.valid := false.B
     s.ar.bits  := 0.U.asTypeOf(s.ar.bits)
@@ -78,7 +75,8 @@ class AXI4LiteCrossBar(
 
   val r_owner_id = RegInit(0.U(idx_width.W))
   r_owner_id := Mux(r_state === r_idle, r_candidate, r_owner_id)
-  val r_owner = slaves(r_owner_id)
+  val r_owner = Wire(new AXI4Lite)
+  r_owner := slaves(r_owner_id(real_width - 1, 0))
 
   r_state := MuxLookup(r_state, r_idle)(
     Seq(
@@ -87,7 +85,7 @@ class AXI4LiteCrossBar(
     )
   )
 
-  def if_rbusy[T <: Data](x: T) = Mux(r_state === r_busy, x, 0.U.asTypeOf(x))
+  def if_rbusy[T <: Data](x: T) = Mux(r_state === r_busy && r_owner_id =/= err_idx, x, 0.U.asTypeOf(x))
 
   // Connect ar
   r_owner.ar.valid := if_rbusy(true.B)
@@ -99,6 +97,15 @@ class AXI4LiteCrossBar(
   master.r.valid  := if_rbusy(r_owner.r.valid)
   r_owner.r.ready := if_rbusy(master.r.ready)
 
+  // Connect DecErr
+  def if_rerr[T <: Data](x: T) = Mux(r_state === r_busy && r_owner_id === err_idx, x, 0.U.asTypeOf(x))
+  dec_err_slave.io.ar.valid := if_rerr(true.B)
+  dec_err_slave.io.ar.bits  := if_rerr(master.ar.bits)
+  master.ar.ready           := if_rerr(dec_err_slave.io.ar.ready)
+  master.r.bits             := if_rerr(dec_err_slave.io.r.bits)
+  master.r.valid            := if_rerr(dec_err_slave.io.r.valid)
+  dec_err_slave.io.r.ready  := if_rerr(master.r.ready)
+
   // Write Crossbar
   val w_idle :: w_busy :: Nil = Enum(2)
 
@@ -108,7 +115,8 @@ class AXI4LiteCrossBar(
 
   val w_owner_id = RegInit(0.U(idx_width.W))
   w_owner_id := Mux(w_state === w_idle, w_candidate, w_owner_id)
-  val w_owner = slaves(w_owner_id)
+  val w_owner = Wire(new AXI4Lite)
+  w_owner := slaves(w_owner_id(real_width - 1, 0))
 
   w_state := MuxLookup(w_state, w_idle)(
     Seq(
@@ -117,7 +125,7 @@ class AXI4LiteCrossBar(
     )
   )
 
-  def if_wbusy[T <: Data](x: T) = Mux(w_state === w_busy, x, 0.U.asTypeOf(x))
+  def if_wbusy[T <: Data](x: T) = Mux(w_state === w_busy && w_owner_id =/= err_idx, x, 0.U.asTypeOf(x))
 
   // Connect aw
   w_owner.aw.valid := if_wbusy(true.B)
@@ -133,4 +141,16 @@ class AXI4LiteCrossBar(
   master.b.bits   := if_rbusy(w_owner.b.bits)
   master.b.valid  := if_rbusy(w_owner.b.valid)
   w_owner.b.ready := if_rbusy(master.b.ready)
+
+  // Connect DecErr
+  def if_werr[T <: Data](x: T) = Mux(w_state === w_busy && w_owner_id === err_idx, x, 0.U.asTypeOf(x))
+  dec_err_slave.io.aw.valid := if_wbusy(true.B)
+  dec_err_slave.io.aw.bits  := if_wbusy(master.aw.bits)
+  master.aw.ready           := if_wbusy(dec_err_slave.io.aw.ready)
+  dec_err_slave.io.w.valid  := if_wbusy(master.w.valid)
+  dec_err_slave.io.w.bits   := if_wbusy(master.w.bits)
+  master.w.ready            := if_wbusy(dec_err_slave.io.w.ready)
+  master.b.bits             := if_rbusy(dec_err_slave.io.b.bits)
+  master.b.valid            := if_rbusy(dec_err_slave.io.b.valid)
+  dec_err_slave.io.b.ready  := if_rbusy(master.b.ready)
 }
