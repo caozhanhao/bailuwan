30e4868c0e0e25d082e721cdba7d5a8860bd5f4d
tracer-ysyx <tracer@ysyx.org>
2025-10-27 21:38:07 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  21:38:07 up  2:18,  1 user,  load average: 2.29, 2.11, 2.16

--- npc/npc/src/Top.scala
+++ npc/npc/src/Top.scala
@@ -30,6 +30,15 @@ class Top extends Module {
   implicit val axi_prop: AXIProperty = AXIProperty()
   val core = Module(new Core)
 
+  core.io.interrupt    := false.B
+  core.io.bus.ar.ready := false.B
+  core.io.bus.aw.ready := false.B
+  core.io.bus.w.ready  := false.B
+  core.io.bus.r.valid  := false.B
+  core.io.bus.b.valid  := false.B
+  core.io.bus.r.bits   := 0.U.asTypeOf(core.io.bus.r.bits)
+  core.io.bus.b.bits   := 0.U.asTypeOf(core.io.bus.b.bits)
+
   // Bore some signals for debugging
   io.registers := BoringUtils.bore(core.RegFile.regs)
   io.pc        := BoringUtils.bore(core.IFU.pc)
