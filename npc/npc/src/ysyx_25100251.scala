0c5307247a0640a7ffefa381ec663df33222ba3d
tracer-ysyx <tracer@ysyx.org>
2025-10-29 19:36:08 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  19:36:08 up 19 min,  1 user,  load average: 1.62, 1.77, 1.34

--- npc/npc/src/ysyx_25100251.scala
+++ npc/npc/src/ysyx_25100251.scala
@@ -4,13 +4,14 @@ import chisel3._
 import amba._
 
 class ysyx_25100251 extends Module {
+  implicit val p:        CoreParams  = CoreParams()
+  implicit val axi_prop: AXIProperty = AXIProperty()
+
   val io = IO(new Bundle {
     val bus       = new AXI4
     val interrupt = Input(Bool())
   })
 
-  implicit val p:        CoreParams  = CoreParams()
-  implicit val axi_prop: AXIProperty = AXIProperty()
   val core = Module(new Core)
 
   core.io.interrupt := io.interrupt
