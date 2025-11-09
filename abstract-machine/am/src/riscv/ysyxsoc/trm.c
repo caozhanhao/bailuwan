8c973d3de05c26b5762de4692bdd950b61fb8ad4
tracer-ysyx <tracer@ysyx.org>
2025-11-09 19:48:26 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  19:48:26 up  8:39,  1 user,  load average: 5.11, 3.31, 2.77

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -8,13 +8,15 @@ int main(const char *args);
 extern char _pmem_start;
 #define PMEM_SIZE (128 * 1024 * 1024)
 #define PMEM_END  ((uintptr_t)&_pmem_start + PMEM_SIZE)
-#define SERIAL_PORT_MMIO 0x10000000
+
+#define UART_BASE 0x10000000L
+#define UART_TX   0
 
 Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
 void putch(char ch) {
-  *(volatile uint8_t*)SERIAL_PORT_MMIO = ch;
+  *(volatile char *)(UART_BASE + UART_TX) = ch;
 }
 
 void halt(int code) {
--- npc/csrc/common/dpic.cpp
+++ npc/csrc/common/dpic.cpp
@@ -11,16 +11,7 @@ void flash_read(int32_t addr, int32_t* data) { assert(0); }
 static uint32_t mrom_data[1024];
 void mrom_read(int32_t addr, int32_t* data)
 {
-    static bool inited = false;
-    if (!inited) {
-        inited = true;
-        FILE* file = fopen("/home/caozhanhao/ysyx/workspace/tmp/chartest.bin", "rb");
-        fread(mrom_data, sizeof(mrom_data), 1, file);
-    }
-
-    *data = mrom_data[(addr - 0x20000000) / 4];
-
-    // printf("mrom_read: addr = 0x%08x, data = 0x%08x\n", addr, *data);
+    *data = sim_handle.get_memory().read(addr);
 }
 
 void ebreak_handler()
