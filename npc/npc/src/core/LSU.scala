f9dc6bb73f9d787bdc8041f24f0b6bf993526363
tracer-ysyx <tracer@ysyx.org>
2025-11-09 15:20:59 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  15:20:59 up  4:12,  1 user,  load average: 4.08, 3.10, 3.05

--- npc/npc/src/core/LSU.scala
+++ npc/npc/src/core/LSU.scala
@@ -23,12 +23,12 @@ class LSU(
 
   // AXI4-Lite
   io.mem.ar.bits.id := 0.U
-  io.mem.ar.bits.len := 1.U
+  io.mem.ar.bits.len := 0.U // burst length=1, equivalent to an AxLEN value of zero.
   io.mem.ar.bits.size := 2.U // 2^2 = 4 bytes
   io.mem.ar.bits.burst := 1.U
 
   io.mem.aw.bits.id := 0.U
-  io.mem.aw.bits.len := 1.U
+  io.mem.aw.bits.len := 0.U // burst length=1, equivalent to an AxLEN value of zero.
   io.mem.aw.bits.size := 2.U // 2^2 = 4 bytes
   io.mem.aw.bits.burst := 1.U
 
