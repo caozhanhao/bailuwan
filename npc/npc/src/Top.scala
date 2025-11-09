46f868ed4fb694a0e4ed6aac0967f74120c70250
tracer-ysyx <tracer@ysyx.org>
2025-11-09 20:44:32 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  20:44:32 up  9:35,  1 user,  load average: 4.40, 4.36, 4.00

--- npc/npc/src/Top.scala
+++ npc/npc/src/Top.scala
@@ -1,29 +1,8 @@
 package top
 
 import chisel3._
-import chisel3.util.experimental.BoringUtils
 import core._
 import amba._
-
-class CSRBoring extends Bundle {
-  val mstatus   = UInt(32.W)
-  val mtvec     = UInt(32.W)
-  val mepc      = UInt(32.W)
-  val mcause    = UInt(32.W)
-  val mcycle    = UInt(32.W)
-  val mcycleh   = UInt(32.W)
-  val mvendorid = UInt(32.W)
-  val marchid   = UInt(32.W)
-}
-
-class DebugBoringBundle extends Bundle {
-  val registers      = Vec(16, UInt(32.W))
-  val pc             = UInt(32.W)
-  val dnpc           = UInt(32.W)
-  val inst           = UInt(32.W)
-  val csrs           = new CSRBoring
-  val difftest_ready = Bool()
-}
 
 class Top extends Module {
   override val desiredName = "ysyx_25100251"
@@ -35,38 +14,10 @@ class Top extends Module {
     val master    = new AXI4
     val slave     = Flipped(new AXI4)
     val interrupt = Input(Bool())
-    val dbg       = Output(new DebugBoringBundle)
   })
 
   val core = Module(new Core)
   core.io.master <> io.master
   core.io.slave <> io.slave
   core.io.interrupt := io.interrupt
-
-  // Bore some signals for debugging
-  io.dbg.registers := BoringUtils.bore(core.RegFile.regs)
-  io.dbg.pc        := BoringUtils.bore(core.IFU.pc)
-  io.dbg.dnpc      := BoringUtils.bore(core.WBU.dnpc)
-  io.dbg.inst      := BoringUtils.bore(core.IDU.inst)
-
-  io.dbg.csrs.mstatus   := BoringUtils.bore(core.EXU.csr_file.mstatus)
-  io.dbg.csrs.mtvec     := BoringUtils.bore(core.EXU.csr_file.mtvec)
-  io.dbg.csrs.mepc      := BoringUtils.bore(core.EXU.csr_file.mepc)
-  io.dbg.csrs.mcause    := BoringUtils.bore(core.EXU.csr_file.mcause)
-  io.dbg.csrs.mcycle    := BoringUtils.bore(core.EXU.csr_file.mcycle).asUInt(31, 0)
-  io.dbg.csrs.mcycleh   := BoringUtils.bore(core.EXU.csr_file.mcycle).asUInt(63, 32)
-  io.dbg.csrs.mvendorid := core.EXU.csr_file.mvendorid
-  io.dbg.csrs.marchid   := core.EXU.csr_file.marchid
-
-  // Difftest got ready after every pc advance (one instruction done),
-  // which is just in.valid delayed one cycle.
-  //               ___________
-  //   ready      |          |
-  //              _____       _____
-  //   clock     |     |_____|     |_____
-  //              cycle 1        cycle 2
-  //                     ^
-  //                     |
-  //          difftest_step is called here
-  io.dbg.difftest_ready := RegNext(BoringUtils.bore(core.IFU.io.in.valid))
 }
