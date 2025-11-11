b52e88e88a2c2acf1dd0de95270b9187074dc81b
tracer-ysyx <tracer@ysyx.org>
2025-11-11 21:30:46 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  21:30:46 up  3:15,  1 user,  load average: 2.13, 2.30, 2.03

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -1,6 +1,9 @@
+#include "../riscv.h"
 #include <am.h>
 #include <klib-macros.h>
 #include <klib.h>
+
+#include <stdint.h>
 
 extern char _heap_start;
 extern char _heap_end;
@@ -14,6 +17,16 @@ int main(const char* args);
 
 #define UART_BASE 0x10000000L
 #define UART_TX   0
+#define UART_THR 0 // w  8
+#define UART_IER 1 // rw 8
+#define UART_IIR 2 // r  8
+#define UART_FCR 2 // w  8
+#define UART_LCR 3 // rw 8
+#define UART_MCR 4 // w  8
+#define UART_LSR 5 // r  8
+#define UART_MSR 6 // r  8
+#define UART_DIVISOR_LATCH_1 0 // rw 8
+#define UART_DIVISOR_LATCH_2 1 // rw 8
 
 Area heap = RANGE(&_heap_start, &_heap_end);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
@@ -31,28 +44,51 @@ __attribute__((noinline)) void halt(int code)
 
 void _trm_init()
 {
+    // Initialize .data section
     size_t data_size = (uintptr_t) & _edata - (uintptr_t) & _sdata;
     for (size_t i = 0; i < data_size; ++i)
         (&_sdata)[i] = (&_sidata)[i];
 
+    // Initialize .bss section
     unsigned int bss_size = (uintptr_t) & _ebss - (uintptr_t) & _sbss;
     for (size_t i = 0; i < bss_size; ++i)
         (&_sbss)[i] = 0;
 
+    // Initialize UART 16550
+    // 1. Set the Line Control Register to the desired line control parameters.
+    //    Set bit 7 to ‘1’ to allow access to the Divisor Latches.
+    uint8_t orignal_LCR = inb(UART_BASE + UART_LCR);
+    outb(UART_BASE + UART_LCR, orignal_LCR | (1 << 7));
+    // 2. Set the Divisor Latches, MSB first, LSB next.
+    outb(UART_BASE + UART_DIVISOR_LATCH_2, 0x00);
+    outb(UART_BASE + UART_DIVISOR_LATCH_1, 0x01);
+    // 3. Set bit 7 of LCR to ‘0’ to disable access to Divisor Latches.
+    //    At this time the transmission engine starts working and data can be sent and received.
+    outb(UART_BASE + UART_LCR, orignal_LCR);
+    // 4. Set the FIFO trigger level.
+    //    Generally, higher trigger level values produce less interrupt to the system,
+    //    so setting it to 14 bytes is recommended if the system responds fast enough.
+    // The Reset Value is 11000000b, which defines FIFO trigger level as 14 bytes, so we
+    // just skip this step.
+    // 5. Enable desired interrupts by setting appropriate bits in the Interrupt Enable register.
+    // We don't need any interrupts. and the Reset Value is already 00h.
 
-    // uint32_t mvendorid, marchid;
-    // asm volatile ("csrr %0, mvendorid" : "=r"(mvendorid));
-    // asm volatile ("csrr %0, marchid" : "=r"(marchid));
+    // Print Welcome Message
+    uint32_t mvendorid, marchid;
+    asm volatile ("csrr %0, mvendorid" : "=r"(mvendorid));
+    asm volatile ("csrr %0, marchid" : "=r"(marchid));
+    
+    char buf[5];
+    buf[0] = (char)((mvendorid >> 24) & 0xFF);
+    buf[1] = (char)((mvendorid >> 16) & 0xFF);
+    buf[2] = (char)((mvendorid >>  8) & 0xFF);
+    buf[3] = (char)( mvendorid        & 0xFF);
+    buf[4] = '\0';
+    printf("[_trm_init]: %s_%d caozhanhao\n", buf, marchid);
 
-    // char buf[5];
-    // buf[0] = (char)((mvendorid >> 24) & 0xFF);
-    // buf[1] = (char)((mvendorid >> 16) & 0xFF);
-    // buf[2] = (char)((mvendorid >>  8) & 0xFF);
-    // buf[3] = (char)( mvendorid        & 0xFF);
-    // buf[4] = '\0';
-
-    // printf("[_trm_init]: %s_%d caozhanhao\n", buf, marchid);
-
+    // Call main
     int ret = main(mainargs);
+
+    // Halt
     halt(ret);
 }
