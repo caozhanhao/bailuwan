014e9926af6b5a01dcd2530c41d18701d42ee304
tracer-ysyx <tracer@ysyx.org>
2025-11-11 19:35:07 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  19:35:07 up  1:19,  1 user,  load average: 4.78, 3.44, 2.93

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -4,7 +4,13 @@
 
 extern char _heap_start;
 extern char _heap_end;
-int main(const char *args);
+extern char _sdata;
+extern char _sidata;
+extern char _edata;
+extern char _sbss;
+extern char _ebss;
+
+int main(const char* args);
 
 #define UART_BASE 0x10000000L
 #define UART_TX   0
@@ -12,29 +18,41 @@ int main(const char *args);
 Area heap = RANGE(&_heap_start, &_heap_end);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
-void putch(char ch) {
-  *(volatile char *)(UART_BASE + UART_TX) = ch;
+void putch(char ch)
+{
+    *(volatile char*)(UART_BASE + UART_TX) = ch;
 }
 
-__attribute__((noinline)) void halt(int code) {
-  asm volatile ("ebreak");
-  while (1);
+__attribute__((noinline)) void halt(int code)
+{
+    asm volatile ("ebreak");
+    while (1);
 }
 
-void _trm_init() {
-  // uint32_t mvendorid, marchid;
-  // asm volatile ("csrr %0, mvendorid" : "=r"(mvendorid));
-  // asm volatile ("csrr %0, marchid" : "=r"(marchid));
+void _trm_init()
+{
+    size_t data_size = (uintptr_t) & _edata - (uintptr_t) & _sdata;
+    for (size_t i = 0; i < data_size; ++i)
+        (&_sdata)[i] = ((char*)&_sidata)[i];
 
-  // char buf[5];
-  // buf[0] = (char)((mvendorid >> 24) & 0xFF);
-  // buf[1] = (char)((mvendorid >> 16) & 0xFF);
-  // buf[2] = (char)((mvendorid >>  8) & 0xFF);
-  // buf[3] = (char)( mvendorid        & 0xFF);
-  // buf[4] = '\0';
+    unsigned int bss_size = (uintptr_t) & _ebss - (uintptr_t) & _sbss;
+    for (size_t i = 0; i < bss_size; ++i)
+        (&_sbss)[i] = 0;
 
-  // printf("[_trm_init]: %s_%d caozhanhao\n", buf, marchid);
 
-  int ret = main(mainargs);
-  halt(ret);
+    // uint32_t mvendorid, marchid;
+    // asm volatile ("csrr %0, mvendorid" : "=r"(mvendorid));
+    // asm volatile ("csrr %0, marchid" : "=r"(marchid));
+
+    // char buf[5];
+    // buf[0] = (char)((mvendorid >> 24) & 0xFF);
+    // buf[1] = (char)((mvendorid >> 16) & 0xFF);
+    // buf[2] = (char)((mvendorid >>  8) & 0xFF);
+    // buf[3] = (char)( mvendorid        & 0xFF);
+    // buf[4] = '\0';
+
+    // printf("[_trm_init]: %s_%d caozhanhao\n", buf, marchid);
+
+    int ret = main(mainargs);
+    halt(ret);
 }
