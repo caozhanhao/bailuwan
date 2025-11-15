82a2efa83d2a33e8a1a1cc0af05cf67c78cb73b7
tracer-ysyx <tracer@ysyx.org>
2025-11-15 22:27:17 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  22:27:17 up  3:23,  2 users,  load average: 3.50, 3.41, 4.37

--- npc/csrc/debug/main.cpp
+++ npc/csrc/debug/main.cpp
@@ -1,25 +1,48 @@
 #include "dut_proxy.hpp"
 #include "utils/disasm.hpp"
 
+#include <getopt.h>
+
+const char* img_file = nullptr;
+
+static void parse_args(int argc, char* argv[])
+{
+    const struct option table[] = {
+        {"batch", no_argument, nullptr, 'b'},
+        {"elf", required_argument, nullptr, 'e'},
+        {"help", no_argument, nullptr, 'h'},
+        {0, 0, nullptr, 0},
+    };
+    int o;
+    while ((o = getopt_long(argc, argv, "-bhe:", table, nullptr)) != -1)
+    {
+        switch (o)
+        {
+        case 'e':
+        case 'b':
+            printf("Ignoring option -%c\n", o);
+            break;
+        case 1:
+            img_file = optarg;
+            return 0;
+        default:
+            printf("Usage: %s [filename]\n", argv[0]);
+            exit(0);
+        }
+    }
+}
+
 int main(int argc, char* argv[])
 {
+    Verilated::commandArgs(argc, argv);
+    parse_args(argc, argv);
     // INIT
-    if (argc != 3)
-    {
-        printf("Usage: %s [number of cycles] [filename]\n", argv[0]);
-        return -1;
-    }
-
-    int cycles = atoi(argv[1]);
-    bool no_cycle_limit = (cycles <= 0);
-
-    sim_handle.init_sim(argv[2]);
+    sim_handle.init_sim(img_file);
     sim_handle.reset(10);
 
     // Simulate
     printf("Simulation started...\n");
-    auto& cpu = sim_handle.get_cpu();
-    while (no_cycle_limit || cycles-- > 0)
+    while (true)
     {
         try
         {
