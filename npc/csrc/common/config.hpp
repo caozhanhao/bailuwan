4bfb5edbd0624ee5d001475ee559e5e3d54e73f5
tracer-ysyx <tracer@ysyx.org>
2025-11-16 22:16:28 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  22:16:28 up 29 min,  1 user,  load average: 3.54, 3.29, 3.13

--- npc/csrc/common/config.hpp
+++ npc/csrc/common/config.hpp
@@ -30,8 +30,8 @@
 #define CONFIG_MTRACE 1
 
 // Only available in sdb:
-// #define CONFIG_ITRACE 1
-// #define CONFIG_FTRACE 1
+#define CONFIG_ITRACE 1
+#define CONFIG_FTRACE 1
 #define CONFIG_WP_BP
 #define CONFIG_DIFFTEST 1
 
