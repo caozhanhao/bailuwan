fe49f55cb96d585b1b8af22fc54f298833971630
tracer-ysyx <tracer@ysyx.org>
2025-11-16 14:07:41 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  14:07:41 up  3:22,  1 user,  load average: 2.32, 2.29, 2.29

--- npc/csrc/sdb/cpu-exec.cpp
+++ npc/csrc/sdb/cpu-exec.cpp
@@ -66,6 +66,12 @@ static void execute(uint64_t n)
 
         if (sdb_state != SDBState::Running) break;
 
+        if (sim_stop_requested)
+        {
+            sim_handle.cleanup();
+            exit(-1);
+        }
+
         // If this instruction is valid and has not been traced, trace it
         // and set the flag.
         if (cpu.is_inst_valid() && !inst_has_been_traced)
@@ -77,12 +83,6 @@ static void execute(uint64_t n)
         // If last instruction done, clear the flag.
         if (cpu.is_ready_for_difftest())
             inst_has_been_traced = false;
-
-        if (sim_stop_requested)
-        {
-            sdb_state = SDBState::Stop;
-            sim_stop_requested = 0;
-        }
     }
 }
 
