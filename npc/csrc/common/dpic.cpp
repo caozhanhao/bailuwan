5efd4002bcc7d6c3b10dc3dcd7d1b1d82efa065f
tracer-ysyx <tracer@ysyx.org>
2025-11-19 22:00:04 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  22:00:04 up 41 min,  1 user,  load average: 3.18, 2.42, 2.23

--- npc/csrc/common/dpic.cpp
+++ npc/csrc/common/dpic.cpp
@@ -40,15 +40,18 @@ int16_t sdram_read(int raddr, char id)
 {
     auto addr = raddr + CONFIG_SDRAM_BASE /* same as flash*/;
     addr += id * 2;
+    int chip_select = (raddr >> 12) & 1;
     auto data = SIM.mem().read<int16_t>(addr);
-    IFDEF(CONFIG_MTRACE, printf("SDRAM Read | id=%d, addr=0x%x, data=0x%x\n", id, raddr, data));
+    IFDEF(CONFIG_MTRACE, printf("SDRAM Read | id=%d, cs=%d, addr=0x%x, data=0x%x\n",
+              id, chip_select, raddr, data));
     return data;
 }
 
 void sdram_write(int waddr, int16_t wdata, char mask, char id)
 {
-    IFDEF(CONFIG_MTRACE, printf("SDRAM Write | id=%d, addr=0x%x, data=0x%x, mask=0x%x\n",
-              id, waddr, wdata, mask));
+    int chip_select = (waddr >> 12) & 1;
+    IFDEF(CONFIG_MTRACE, printf("SDRAM Write | id=%d, cs=%d, addr=0x%x, data=0x%x, mask=0x%x\n",
+              id, chip_select, waddr, wdata, mask));
     auto addr = waddr + CONFIG_SDRAM_BASE /* same as flash*/;
     addr += id * 2;
     SIM.mem().write<int16_t>(addr, wdata, mask);
