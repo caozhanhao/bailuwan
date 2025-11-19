a93271fe5779e19743639ed35f5b83610ed96346
caozhanhao <cao2013zh@163.com>
2025-11-19 22:41:03 +0800

    npc: fix SDRAM.

--- npc/csrc/common/dpic.cpp
+++ npc/csrc/common/dpic.cpp
@@ -36,21 +36,27 @@ void psram_write(int waddr, char wdata)
     return SIM.mem().write<char>(waddr + CONFIG_PSRAM_BASE /* same as flash*/, wdata, 0b1);
 }
 
+// The SDRAM addr only has low 25-bit valid.
+// One chip -> 25-bit -> 32 MB
+// id is used to tell which chip is being accessed.
 int16_t sdram_read(int raddr, char id)
 {
+    assert(id >= 0 && id < CONFIG_SDRAM_CHIP_NUM);
     auto addr = raddr + CONFIG_SDRAM_BASE /* same as flash*/;
-    addr += id * 2;
+    addr += id * CONFIG_SDRAM_CHIP_SIZE;
     auto data = SIM.mem().read<int16_t>(addr);
-    IFDEF(CONFIG_MTRACE, printf("SDRAM Read | id=%d, addr=0x%x, data=0x%x\n", id, raddr, data));
+    IFDEF(CONFIG_MTRACE, printf("SDRAM Read | id=%d, addr=0x%x, data=0x%x\n",
+              id, raddr, data));
     return data;
 }
 
 void sdram_write(int waddr, int16_t wdata, char mask, char id)
 {
+    assert(id >= 0 && id < CONFIG_SDRAM_CHIP_NUM);
     IFDEF(CONFIG_MTRACE, printf("SDRAM Write | id=%d, addr=0x%x, data=0x%x, mask=0x%x\n",
               id, waddr, wdata, mask));
     auto addr = waddr + CONFIG_SDRAM_BASE /* same as flash*/;
-    addr += id * 2;
+    addr += id * CONFIG_SDRAM_CHIP_SIZE;
     SIM.mem().write<int16_t>(addr, wdata, mask);
 }
 
