338be55775de500baa91b840835a2949da3cd9e7
tracer-ysyx <tracer@ysyx.org>
2025-11-20 23:06:53 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  23:06:53 up  4:44,  1 user,  load average: 4.34, 3.25, 2.87

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -56,7 +56,7 @@ void init_uart16550()
 
     // 2. Set the Divisor Latches, MSB first, LSB next.
     outb(UART_BASE + UART_DIVISOR_LATCH_2, 0x00);
-    outb(UART_BASE + UART_DIVISOR_LATCH_1, 0xf1);
+    outb(UART_BASE + UART_DIVISOR_LATCH_1, 0x01);
 
     // 3. Set bit 7 of LCR to ‘0’ to disable access to Divisor Latches.
     //    At this time the transmission engine starts working and data can be sent and received.
