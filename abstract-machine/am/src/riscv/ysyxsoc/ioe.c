06ff5256b1c6c2c585d522acd3b75993e8453944
tracer-ysyx <tracer@ysyx.org>
2025-11-21 15:25:05 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  15:25:05 up  1:04,  1 user,  load average: 4.43, 3.42, 2.96

--- abstract-machine/am/src/riscv/ysyxsoc/ioe.c
+++ abstract-machine/am/src/riscv/ysyxsoc/ioe.c
@@ -12,6 +12,7 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *);
 
 static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
 static void __am_uart_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
+static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = false;  }
 
 // similar to putch in trm.c
 static void __am_uart_tx(AM_UART_TX_T* tx)
@@ -34,6 +35,7 @@ static void *lut[128] = {
   [AM_TIMER_CONFIG] = __am_timer_config,
   [AM_TIMER_RTC   ] = __am_timer_rtc,
   [AM_TIMER_UPTIME] = __am_timer_uptime,
+  [AM_INPUT_CONFIG] = __am_input_config,
   [AM_INPUT_KEYBRD] = __am_input_keybrd,
 };
 
