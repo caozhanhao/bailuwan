dc56aa761cbfc81ee20787090527383a58cc2671
tracer-ysyx <tracer@ysyx.org>
2025-11-22 18:04:25 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  18:04:25 up  7:54,  1 user,  load average: 3.08, 2.46, 1.93

--- npc/npc/src/core/CLINT.scala
+++ npc/npc/src/core/CLINT.scala
@@ -17,23 +17,12 @@ class CLINT(
   val mtime = RegInit(0.U(64.W))
   mtime := mtime + 1.U
 
-  val r_idle :: r_wait_ready :: Nil = Enum(2)
+  val addr_reg = RegInit(0.U(axi_prop.ADDR_WIDTH.W))
+  addr_reg := Mux(io.ar.fire, io.ar.bits.addr, addr_reg)
 
-  val r_state = RegInit(r_idle)
-
-  r_state := MuxLookup(r_state, r_idle)(
-    Seq(
-      r_idle       -> Mux(io.ar.fire, r_wait_ready, r_idle),
-      r_wait_ready -> Mux(io.r.fire, r_idle, r_wait_ready)
-    )
-  )
-
-  val addr = RegInit(0.U(axi_prop.ADDR_WIDTH.W))
-  addr := Mux(r_state === r_idle, io.ar.bits.addr, addr)
-
-  io.ar.ready    := r_state === r_idle
-  io.r.valid     := r_state === r_wait_ready
-  io.r.bits.data := Mux(addr === 0xa0000048L.U, mtime(31, 0), mtime(63, 32))
+  io.ar.ready    := true.B
+  io.r.valid     := true.B
+  io.r.bits.data := Mux(addr_reg === 0x02000000L.U, mtime(31, 0), mtime(63, 32))
   io.r.bits.resp := AXIResp.OKAY
 
   io.aw.ready    := true.B
