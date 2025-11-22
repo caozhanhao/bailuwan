01e6a99fe104c39d728b16d0f1187b4de5912917
tracer-ysyx <tracer@ysyx.org>
2025-11-23 00:06:06 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  00:06:06 up 13:56,  1 user,  load average: 3.84, 4.05, 4.21

--- npc/sim/common/dut_proxy.cpp
+++ npc/sim/common/dut_proxy.cpp
@@ -175,9 +175,9 @@ void CPUProxy::dump_perf_counters(FILE* stream)
 
     auto all_ops_d = static_cast<double>(*b.all_ops);
 
-    fprintf(stream, "+----------+------------+--------+------------+\n");
-    fprintf(stream, "| Name     | Count      | %%      | Avg Cycles |\n");
-    fprintf(stream, "+----------+------------+--------+------------+\n");
+    fprintf(stream, "+----------+----------+--------+------------+\n");
+    fprintf(stream, "| Name     | Count    | %%      | Avg Cycles |\n");
+    fprintf(stream, "+----------+----------+--------+------------+\n");
 #define PERF(name)  fprintf(stream, "| %-8s | %8lu | %05.2f%% | %10.2f |\n", \
     STRINGIFY(name), \
     *b.name##_ops, \
@@ -190,7 +190,7 @@ void CPUProxy::dump_perf_counters(FILE* stream)
     PERF(csr);
     PERF(other);
 #undef PERF
-    fprintf(stream, "+----------+------------+--------+------------+\n");
+    fprintf(stream, "+----------+----------+--------+------------+\n");
 }
 
 void CPUProxy::dump(FILE* stream)
