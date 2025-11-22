7974ead7d748c9c3859a976bd4998eddf4494b5f
tracer-ysyx <tracer@ysyx.org>
2025-11-22 12:52:45 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  12:52:45 up  2:43,  1 user,  load average: 3.38, 3.23, 3.30

--- abstract-machine/am/src/riscv/ysyxsoc/input.c
+++ abstract-machine/am/src/riscv/ysyxsoc/input.c
@@ -1,6 +1,43 @@
+#include "../riscv.h"
+
 #include <am.h>
+#include <amdev.h>
+#include <klib-macros.h>
+
+#define PS2_KEYBOARD_BASE 0x10011000
+
+#define AM_KEY_ENTRY(name, code, arg) [code] = AM_KEY_##name,
+#define AM_EXT_KEY_ENTRY(name, code, arg)
+static int keymap[256] = {
+  AM_KEY_TABLE(AM_KEY_ENTRY, AM_EXT_KEY_ENTRY, 0)
+};
+#undef AM_KEY_ENTRY
+#undef AM_EXT_KEY_ENTRY
+
+#define AM_EXT_KEY_ENTRY(name, code, arg) [code] = AM_KEY_##name,
+#define AM_KEY_ENTRY(name, code, arg)
+static int keymap_ext[256] = {
+  AM_KEY_TABLE(AM_KEY_ENTRY, AM_EXT_KEY_ENTRY, 0)
+};
+#undef AM_KEY_ENTRY
+#undef AM_EXT_KEY_ENTRY
+
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  bool keydown = false;
+  bool ext = false;
+  uint8_t code;
+  while ((code = inb(PS2_KEYBOARD_BASE)))
+  {
+    if (code == 0xF0)
+      keydown = false;
+    else if (code == 0xE0)
+      ext = true;
+    else
+    {
+      kbd->keycode = ext ? keymap_ext[code] : keymap[code];
+      kbd->keydown = keydown;
+      return;
+    }
+  }
 }
