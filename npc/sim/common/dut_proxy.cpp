294ac0ce7913e15466579593457872b753f74de5
tracer-ysyx <tracer@ysyx.org>
2025-11-23 00:09:59 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  00:09:59 up 14:00,  1 user,  load average: 3.27, 3.93, 4.15

--- npc/sim/common/dut_proxy.cpp
+++ npc/sim/common/dut_proxy.cpp
@@ -171,6 +171,8 @@ void CPUProxy::dump_perf_counters(FILE* stream)
     PERF(ifu_fetched);
     PERF(lsu_read);
     PERF(exu_done);
+    PERF(all_ops);
+    PERF(all_cycles);
 #undef PERF
 
     auto all_ops_d = static_cast<double>(*b.all_ops);
