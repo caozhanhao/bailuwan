e1f62a7dc52d73bccc6c60b136e40d234c766638
tracer-ysyx <tracer@ysyx.org>
2025-11-29 00:46:14 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  00:46:14 up  2:33,  1 user,  load average: 5.65, 5.88, 5.73

--- npc/sim/common/dut_proxy.cpp
+++ npc/sim/common/dut_proxy.cpp
@@ -19,7 +19,7 @@ const char* gpr_names[32] = {
 
 void CPUProxy::bind(TOP_NAME* this_dut)
 {
-    auto scope_map = Verilated::scopeNameMap();
+    auto scope_map = this_dut->contextp()->scopeNameMap();
     assert(scope_map && "Verilated::scopeNameMap() == nullptr");
 
     auto find_signal = [&](const std::string& target) -> void*
