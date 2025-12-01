5587532af3c9ece58e661de10eaf99b65fa5a525
tracer-ysyx <tracer@ysyx.org>
2025-12-01 20:21:08 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  20:21:08 up  1:01,  1 user,  load average: 4.23, 4.07, 4.46

--- npc/sim/common/utils/macro.hpp
+++ npc/sim/common/utils/macro.hpp
@@ -46,8 +46,8 @@
 // keep the code if a boolean macro is defined to 0
 #define IFZERO(macro, ...) MUXZERO(macro, __KEEP, __IGNORE)(__VA_ARGS__)
 
-#define STRINGIFY(s)        #s
-#define TOSTRING(s)         STRINGIFY(s)
+#define TOSTRING__(s)        #s
+#define TOSTRING(s)         TOSTRING__(s)
 
 #define ANSI_FG_BLACK   "\33[1;30m"
 #define ANSI_FG_RED     "\33[1;31m"
