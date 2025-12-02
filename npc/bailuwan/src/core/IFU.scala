b9eebb3fc49811640c02d22b0c6195c136bf0638
tracer-ysyx <tracer@ysyx.org>
2025-12-02 17:20:45 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  17:20:45 up  3:55,  1 user,  load average: 3.53, 3.42, 3.51

--- npc/bailuwan/src/core/IFU.scala
+++ npc/bailuwan/src/core/IFU.scala
@@ -16,15 +16,57 @@ class IFUOut(
   val inst = Output(UInt(32.W))
 }
 
+class ICacheReq(
+  implicit p: CoreParams)
+    extends Bundle {
+  val addr = UInt(p.XLEN.W)
+}
+
+class ICacheResp(
+  implicit p: CoreParams)
+    extends Bundle {
+  val data  = UInt(32.W)
+  val error = Bool()
+}
+
+class ICacheIO(
+  implicit p: CoreParams)
+    extends Bundle {
+  val req  = Decoupled(new ICacheReq)
+  val resp = Flipped(Decoupled(new ICacheResp))
+}
+
 class ICache(
   implicit p: CoreParams,
   axi_prop:   AXIProperty)
     extends Module {
   val io = IO(new Bundle {
-    val ifu = Flipped(new AXI4())
+    val ifu = new ICacheIO()
     val mem = new AXI4()
   })
-  io.ifu <> io.mem
+
+  val req  = io.ifu.req
+  val resp = io.ifu.resp
+
+  io.mem.ar.valid     := req.valid
+  req.ready           := io.mem.ar.ready
+  io.mem.ar.bits.addr := req.bits.addr
+
+  io.mem.r.ready  := resp.ready
+  resp.valid      := io.mem.r.valid
+  resp.bits.data  := io.mem.r.bits.data
+  resp.bits.error := io.mem.r.bits.resp =/= AXIResp.OKAY
+
+  io.mem.ar.bits.id    := 0.U
+  io.mem.ar.bits.len   := 0.U // burst length=1, equivalent to an AxLEN value of zero.
+  io.mem.ar.bits.size  := 2.U // 2^2 = 4 bytes
+  io.mem.ar.bits.burst := 0.U
+  io.mem.aw.valid      := false.B
+  io.mem.aw.bits       := DontCare
+  io.mem.w.valid       := false.B
+  io.mem.w.bits        := DontCare
+  io.mem.b.ready       := false.B
+  io.mem.w.bits.last   := true.B
 }
 
 class IFU(
@@ -38,44 +80,32 @@ class IFU(
     val mem = new AXI4()
   })
 
-  val icache = Module(new ICache)
+  val icache    = Module(new ICache)
+  val icache_io = icache.io.ifu
+
   icache.io.mem <> io.mem
-
-  val reader = icache.io.ifu
-
-  reader.ar.bits.id    := 0.U
-  reader.ar.bits.len   := 0.U // burst length=1, equivalent to an AxLEN value of zero.
-  reader.ar.bits.size  := 2.U // 2^2 = 4 bytes
-  reader.ar.bits.burst := 0.U
-
-  reader.aw.valid    := false.B
-  reader.aw.bits     := DontCare
-  reader.w.valid     := false.B
-  reader.w.bits      := DontCare
-  reader.b.ready     := false.B
-  reader.w.bits.last := true.B
 
   val s_idle :: s_wait_mem :: s_wait_ready :: s_fault :: Nil = Enum(4)
 
   val state = RegInit(s_idle)
   state := MuxLookup(state, s_idle)(
     Seq(
-      s_idle       -> Mux(reader.ar.fire, s_wait_mem, s_idle),
-      s_wait_mem   -> Mux(reader.r.fire, Mux(reader.r.bits.resp === AXIResp.OKAY, s_wait_ready, s_fault), s_wait_mem),
+      s_idle       -> Mux(icache_io.req.fire, s_wait_mem, s_idle),
+      s_wait_mem   -> Mux(icache_io.resp.fire, Mux(icache_io.resp.bits.error, s_fault, s_wait_ready), s_wait_mem),
       s_wait_ready -> Mux(io.out.fire, s_idle, s_wait_ready)
     )
   )
 
-  reader.ar.valid := (state === s_idle) && !reset.asBool // Don't send request when resetting
-  reader.r.ready  := state === s_wait_mem
+  icache_io.req.valid  := (state === s_idle) && !reset.asBool // Don't send request when resetting
+  icache_io.resp.ready := state === s_wait_mem
 
   val pc = RegInit(p.ResetVector.S(p.XLEN.W).asUInt)
   pc := Mux(io.in.fire, io.in.bits.dnpc, pc)
 
-  reader.ar.bits.addr := pc
+  icache_io.req.bits.addr := pc
 
   val inst_reg = RegInit(0.U(32.W))
-  inst_reg := Mux(reader.r.fire, reader.r.bits.data, inst_reg)
+  inst_reg := Mux(icache_io.resp.fire, icache_io.resp.bits.data, inst_reg)
 
   io.out.bits.inst := inst_reg
   io.out.bits.pc   := pc
@@ -84,11 +114,9 @@ class IFU(
   io.out.valid := state === s_wait_ready
 
   val fault_addr = RegInit(0.U(p.XLEN.W))
-  fault_addr := Mux(reader.ar.fire, pc, fault_addr)
-  val fault_resp = RegInit(AXIResp.OKAY)
-  fault_resp := Mux(reader.r.fire, reader.r.bits.resp, fault_resp)
+  fault_addr := Mux(icache_io.req.fire, pc, fault_addr)
 
-  assert(state =/= s_fault, cf"IFU: Access fault at 0x${fault_addr}%x, resp=${fault_resp}")
+  assert(state =/= s_fault, cf"IFU: Access fault at 0x${fault_addr}%x")
 
   // Difftest got ready after every pc advance (one instruction done),
   // which is just in.valid delayed one cycle.
@@ -103,5 +131,5 @@ class IFU(
   SignalProbe(RegNext(io.in.valid), "difftest_ready")
   SignalProbe(pc, "pc")
   SignalProbe(state, "ifu_state")
-  PerfCounter(reader.r.fire, "ifu_fetched")
+  PerfCounter(icache_io.resp.fire, "ifu_fetched")
 }
