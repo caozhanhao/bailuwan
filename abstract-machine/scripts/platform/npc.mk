e572e4fb6b0e8a2c042cddd87a4d36f6ac69cc04
tracer-ysyx <tracer@ysyx.org>
2025-12-02 14:44:11 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  14:44:11 up  1:18,  1 user,  load average: 5.09, 4.44, 3.74

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -31,6 +31,6 @@ image: image-dep
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: insert-arg
-	$(MAKE) -C $(NPC_HOME) sim ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin HW=$(NPC_HW) TRACE=$(NPC_TRACE)
+	$(MAKE) -C $(NPC_HOME) sim TOPNAME=ysyx_25100251 ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin HW=$(NPC_HW) TRACE=$(NPC_TRACE)
 
 .PHONY: insert-arg
