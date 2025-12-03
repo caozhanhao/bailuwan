e8abb403020965845c1026e9ded5191b093270f6
tracer-ysyx <tracer@ysyx.org>
2025-12-03 20:48:31 +0800

    > compile NEMU ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  20:48:31 up  1:28,  1 user,  load average: 2.52, 2.90, 2.93

--- npc/cachesim/main.cpp
+++ npc/cachesim/main.cpp
@@ -125,9 +125,14 @@ int main(int argc, char* argv[])
 
     init(image, bytes_read);
 
-    ICacheSim sim(64, 4, 1);
-    drain_pc_stream([&sim](uint32_t pc) { sim.step(pc); });
-    sim.dump(stdout);
+    drain_pc_stream([](uint32_t pc)
+    {
+        printf("0x%x\n", pc);
+    });
+
+    // ICacheSim sim(64, 4, 1);
+    // drain_pc_stream([&sim](uint32_t pc) { sim.step(pc); });
+    // sim.dump(stdout);
 
     free(image);
     return 0;
