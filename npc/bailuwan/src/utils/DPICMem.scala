a118d13cb0b0ef2243c686c162acade9cc2b7fd3
tracer-ysyx <tracer@ysyx.org>
2025-12-09 14:45:28 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  14:45:28 up  2:24,  1 user,  load average: 5.54, 4.29, 3.70

--- npc/bailuwan/src/utils/DPICMem.scala
+++ npc/bailuwan/src/utils/DPICMem.scala
@@ -89,13 +89,14 @@ class DPICMem(
   val r_addr = RegInit(0.U(32.W))
   val r_cnt  = RegInit(0.U(8.W))
 
-  r_addr := Mux(io.ar.fire, io.ar.bits.addr, Mux(io.r.fire, next_addr(r_addr, r_ctx.size, r_ctx.burst), r_addr))
+  val next_addr = Mux(io.ar.fire, io.ar.bits.addr, Mux(io.r.fire, next_addr(r_addr, r_ctx.size, r_ctx.burst), r_addr))
+  r_addr := next_addr
 
   r_cnt := Mux(io.ar.fire, 0.U, Mux(io.r.fire, r_cnt + 1.U, r_cnt))
 
   io.r.bits.last := r_cnt === r_ctx.len
 
-  mem_read.io.addr := r_addr
+  mem_read.io.addr := next_addr
   mem_read.io.en   := ((io.ar.fire && r_state === r_idle) || (r_state === r_reading)) && !reset.asBool
 
   io.r.bits.data := mem_read.io.out
