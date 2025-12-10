56d67709e837f7a7c595c44e482c7beaea74644c
tracer-ysyx <tracer@ysyx.org>
2025-12-11 00:20:31 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  00:20:31 up 13:44,  1 user,  load average: 2.42, 2.47, 2.65

--- npc/bailuwan/src/core/IFU.scala
+++ npc/bailuwan/src/core/IFU.scala
@@ -202,7 +202,7 @@ class IFU(
   io.out.bits.inst := inst_reg
   io.out.bits.pc   := pc
 
-  io.in.ready  := state === s_idle
+  io.in.ready  := true.B
   io.out.valid := state === s_wait_ready
 
   assert(
