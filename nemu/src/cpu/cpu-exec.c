244a676a4caf47a93876759c5bc3d5152cd3b98d
tracer-ysyx <tracer@ysyx.org>
2025-12-11 14:59:35 +0800

    > compile NEMU ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  14:59:35 up  5:56,  1 user,  load average: 5.61, 4.26, 3.77

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -100,7 +100,7 @@ void iringbuf_display() {
 #endif
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
-#define CONFIG_ITRACE_COND
+  log_write("%s\n", _this->logbuf);
 #ifdef CONFIG_ITRACE_COND
   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
 #endif
