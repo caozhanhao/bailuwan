c8c2a4c431315acc0ec1edc75a78dd3571175220
tracer-ysyx <tracer@ysyx.org>
2025-12-14 23:15:10 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  23:15:10 up 11:58,  2 users,  load average: 10.80, 6.63, 4.43

--- npc/sim/common/dut_proxy.cpp
+++ npc/sim/common/dut_proxy.cpp
@@ -144,10 +144,10 @@ void CPUProxy::dump_perf_counters(FILE* stream)
     PERF(all_cycles);
     PERF(icache_hit);
     PERF(icache_miss);
-    PERF(idu_hazard_stall_cycles);
 #undef PERF
 
     auto all_ops_d = static_cast<double>(*b.all_ops);
+    auto all_cycles_d = static_cast<double>(*b.all_cycles);
 
     fprintf(stream, "+----------+----------+--------+\n");
     fprintf(stream, "| Type     |    Count | %%      |\n");
@@ -173,6 +173,10 @@ void CPUProxy::dump_perf_counters(FILE* stream)
     fprintf(stream, "icache hit rate = %f\n", hit_rate);
     fprintf(stream, "icache miss penalty = %f\n", miss_penalty);
     fprintf(stream, "icache AMAT = %f\n", AMAT);
+
+    auto idu_hazard = *b.idu_hazard_stall_cycles;
+    auto idu_hazard_percent = static_cast<double>(idu_hazard) / all_cycles_d;
+    fprintf(stream, "IDU hazard stalled = %lu (%f %%)\n", idu_hazard, idu_hazard_percent);
 #endif
 }
 
@@ -266,7 +270,7 @@ void DUTMemory::out_of_bound_abort(uint32_t addr)
 {
     auto& cpu = SIM.cpu();
     printf("Out of bound memory access at addr=0x%08x\n, ifu_pc=0x%08x, lsu_pc=0x%08x",
-        addr, cpu.ifu_pc(), cpu.lsu_pc());
+           addr, cpu.ifu_pc(), cpu.lsu_pc());
     cpu.dump();
     SIM.cleanup();
     exit(-1);
