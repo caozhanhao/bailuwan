c848e67f5d814c2336bf6570a1a9d1cdb0f55e80
tracer-ysyx <tracer@ysyx.org>
2025-12-14 11:51:43 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  11:51:43 up 35 min,  1 user,  load average: 3.56, 3.11, 2.65

--- npc/bailuwan/src/core/EXU.scala
+++ npc/bailuwan/src/core/EXU.scala
@@ -191,7 +191,7 @@ class EXU(
   // Expose pc and inst in EXU to avoid the testbench see the flushed instructions
   SignalProbe(decoded.pc, "pc")
   SignalProbe(decoded.inst, "inst")
-  SignalProbe(io.in.valid, "inst_valid")
+  SignalProbe(io.in.fire, "inst_valid")
 
   PerfCounter(io.out.valid, "exu_done")
 }
