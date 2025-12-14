d64262c2b7eaf0103de9807b67e581dd5e9ea664
tracer-ysyx <tracer@ysyx.org>
2025-12-14 19:54:22 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  19:54:22 up  8:37,  1 user,  load average: 1.89, 2.45, 2.23

---
+++ npc/bailuwan/src/constants/Exception.scala
@@ -0,0 +1,24 @@
+// Copyright (c) 2025 caozhanhao
+// SPDX-License-Identifier: MIT
+
+package constants
+
+import chisel3._
+import chisel3.util._
+
+object ExceptionCode {
+  val InstructionAddressMisaligned = 0.U
+  val InstructionAccessFault = 1.U
+  val IllegalInstruction = 2.U
+  val Breakpoint = 3.U
+  val LoadAddressMisaligned = 4.U
+  val LoadAccessFault = 5.U
+  val StoreAMOAddressMisaligned = 6.U
+  val StoreAMOFault = 7.U
+  val EnvironmentCallFromUMode = 8.U
+  val EnvironmentCallFromSMode = 9.U
+  val EnvironmentCallFromMMode = 11.U
+  val InstructionPageFault = 12.U
+  val LoadPageFault = 13.U
+  val StoreAMOPageFault = 15.U
+}
