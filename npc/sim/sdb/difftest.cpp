395ad12dc585de8a5359b6eb0cfab08d21db2869
tracer-ysyx <tracer@ysyx.org>
2025-12-15 23:56:09 +0800

    > sim RTL ysyx_25100251 曹展豪 Linux caozhanhao-achnoz 6.12.48-1-MANJARO #1 SMP PREEMPT_DYNAMIC Fri, 19 Sep 2025 16:11:04 +0000 x86_64 GNU/Linux  23:56:09 up 12:38,  1 user,  load average: 2.79, 2.99, 3.52

--- npc/sim/sdb/difftest.cpp
+++ npc/sim/sdb/difftest.cpp
@@ -173,8 +173,10 @@ static bool is_accessing_device()
 
 void difftest_step()
 {
-    // fprintf(stderr, "DIFF_STEP, 0x%x: %s\n", SIM.cpu().wbu_pc(),
-    //         rv32_disasm(SIM.cpu().wbu_pc(), SIM.cpu().wbu_inst()).c_str());
+    IFDEF(CONFIG_DIFFTEST_TRACE,
+          fprintf(stderr, "DIFF_STEP, 0x%x: %s\n", SIM.cpu().wbu_pc(),
+              rv32_disasm(SIM.cpu().wbu_pc(), SIM.cpu().wbu_inst()).c_str())
+    );
 
     if (is_accessing_device())
     {
